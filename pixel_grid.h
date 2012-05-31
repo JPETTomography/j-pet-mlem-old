4199162d8eadee8b67ad6e0878c344347327ef9a
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-05-31 21:35:53 +0200

    added test of pixel grid

--- pixel_grid.h
+++ pixel_grid.h
@@ -23,7 +23,7 @@ public:
 
   PixelGrid(const Point<F> &ll, const Point<F> &ur, I nx, I ny):
     nx_(nx),ny_(ny),
-    pixels_(nx_*ny_) {
+    pixels_(nx_*ny_,(F)0.0) {
     ll_x_=ll.x;
     ll_y_=ll.y;
     ur_x_=ur.x;
@@ -46,26 +46,47 @@ public:
   F dy() const {return dy_;}
 
 
-  Point<F> center(I ix, I iy) const;
-  
+  Point<F> center(I ix, I iy) const  {
+    return Point<F>(ll_x_+(ix+0.5)*dx_,ll_y_+(iy+0.5)*dy_);
+  }
+  Point<F> center(const Index<I> &ind) const  {
+    return center(ind.x,ind.y);
+  }
+
   Index<I> in(F x, F y) const {
     I ix=(int) floor( (x-ll_x_)/dx_);
     I iy=(int) floor( (y-ll_y_)/dy_);
     return Index<I>(ix,iy);
   };
+
+  Index<I> in(const Point<F> &p) const {
+    return in(p.x,p.y);
+  };
   
+  void add(I ix, I iy, F w=1.0) {
+    pixels_[index(ix,iy)]+=w;
+  }
   
-  void add(I ix, I iy, F w) {
-    
+  void add(Index<I> ind, F w=1.0) {
+    add(ind.x,ind.y,w);
   }
 
   void insert(F x, F y, F w) {
-    
+    add(in(x,y),w);
   }
-  
+
+  void insert(const Point<F> &p, F w) {
+    add(in(p),w);
+  }
+
   I index(I ix, I iy) const {return iy*nx_+ix;}
   I index(Index<F> ind) const {return index(ind.x,ind.y);}
-  
+
+
+  F operator()(I i) const {return pixels_[i];}
+  F operator[](I i) const {return this->operator()(i);}
+  F operator()(I ix,I iy) const {return pixels_[index(ix,iy)];}
+
 private:
   F  ll_x_, ll_y_;
   F  ur_x_, ur_y_;
--- pixel_grid_test.cpp
+++ pixel_grid_test.cpp
@@ -69,3 +69,124 @@ TEST_F(pixel_grid_test,index_test) {
 
   
 }
+
+TEST_F(pixel_grid_test,center_test) {
+  {
+    Point<> c=grid.center(0,0);
+  
+    ASSERT_DOUBLE_EQ(-247.5,c.x);
+    ASSERT_DOUBLE_EQ(-199.0,c.y);
+  }
+  {
+    Point<> c=grid.center(50,70);
+    
+    ASSERT_DOUBLE_EQ(  2.5, c.x);
+    ASSERT_DOUBLE_EQ(-59.0, c.y);
+  }
+
+}
+
+
+TEST_F(pixel_grid_test,in_test) {
+  {
+    Point<> c=grid.center(0,0);
+  
+    ASSERT_DOUBLE_EQ(-247.5,c.x);
+    ASSERT_DOUBLE_EQ(-199.0,c.y);
+
+    Index<> ind=grid.in(c);
+    ASSERT_EQ(0,ind.x);
+    ASSERT_EQ(0,ind.y);
+    
+  }
+  {
+    Point<> c=grid.center(50,70);
+    
+    ASSERT_DOUBLE_EQ(  2.5, c.x);
+    ASSERT_DOUBLE_EQ(-59.0, c.y);
+
+    Index<> ind=grid.in(c);
+    ASSERT_EQ(50,ind.x);
+    ASSERT_EQ(70,ind.y);
+    
+  }
+
+    {
+    Point<> c=grid.center(0,0);
+  
+    ASSERT_DOUBLE_EQ(-247.5,c.x);
+    ASSERT_DOUBLE_EQ(-199.0,c.y);
+
+    Index<> ind=grid.in(Point<>(c.x+1.75, c.y-0.999) );
+    ASSERT_EQ(0,ind.x);
+    ASSERT_EQ(0,ind.y);
+    
+  }
+  {
+    Point<> c=grid.center(50,70);
+    
+    ASSERT_DOUBLE_EQ(  2.5, c.x);
+    ASSERT_DOUBLE_EQ(-59.0, c.y);
+
+
+    Index<> ind=grid.in(Point<>(c.x-2.4999, c.y+0.999) );
+    ASSERT_EQ(50,ind.x);
+    ASSERT_EQ(70,ind.y);
+    
+  }
+
+    {
+    Point<> c=grid.center(50,70);
+    
+    ASSERT_DOUBLE_EQ(  2.5, c.x);
+    ASSERT_DOUBLE_EQ(-59.0, c.y);
+
+
+    Index<> ind=grid.in(Point<>(c.x-2.4999, c.y+1.0001) );
+    ASSERT_EQ(50,ind.x);
+    ASSERT_NE(70,ind.y);
+    
+  }
+
+}
+
+
+TEST_F(pixel_grid_test,add_test) {
+  grid.add(0,0,0.5);
+  
+  ASSERT_DOUBLE_EQ(0.5, grid[0]); 
+  grid.add(0,0,0.76);
+  ASSERT_DOUBLE_EQ(1.26, grid[0]); 
+
+  grid.add(13,23);
+  ASSERT_EQ(1.0,grid(23*70+13));
+
+}
+
+
+TEST_F(pixel_grid_test,insert_test) {
+  {
+    Point<> c=grid.center(0,0);
+  
+    ASSERT_DOUBLE_EQ(-247.5,c.x);
+    ASSERT_DOUBLE_EQ(-199.0,c.y);
+
+    grid.insert(-247.5,-199.0,0.5);
+    
+    ASSERT_DOUBLE_EQ(0.5,grid(0,0));
+    
+    
+  }
+  {
+    Point<> c=grid.center(50,70);
+    
+    ASSERT_DOUBLE_EQ(  2.5, c.x);
+    ASSERT_DOUBLE_EQ(-59.0, c.y);
+
+    grid.insert(2.5,-59.0,0.5);
+    
+    ASSERT_DOUBLE_EQ(0.5,grid(50,70));
+    
+    
+  }
+}
