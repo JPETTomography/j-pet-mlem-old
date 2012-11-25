3251808642802fd356da3c6b154d152c58b5e71c
Jakub Kowal <kowaljakub@gmail.com>
2012-11-25 15:27:16 +0100

    Last version of EMT algorithm(not-working).

---
+++ src/pet_rec.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <vector>
+#include <list>
+
+#define LOCATION(x,y,size) (x*size) + y
+
+typedef std::pair<size_t, size_t> pixel_location;
+typedef uint32_t file_int;
+typedef uint16_t file_half;
+
+ 
+ #define fourcc(a, b, c, d) (((d)<<24) | ((c)<<16) | ((b)<<8) | (a))
+ 
+// binary serialization                                  // n_pixels  n_detectors  triagular
+static const file_int magic_1 = fourcc('P','E','T','t'); //                           X
+static const file_int magic_2 = fourcc('P','E','T','s'); //     X                     X
+static const file_int magic_t = fourcc('P','E','T','p'); //     X          X          X
+static const file_int magic_f = fourcc('P','E','T','P'); //     X          X
+
+
+using namespace std;
+
+struct hits_per_pixel{
+
+	pixel_location location;
+	float probability;
+
+
+};
+
+
+class Pet_reconstruction{
+
+private:
+	int n_tubes;
+	int n_pixels;
+	int n_iter;
+	int emissions;
+	int n_lors;
+	static vector< std::list<hits_per_pixel> > system_matrix;
+	static vector<float> n;
+	float *scale;
+
+
+public:
+	Pet_reconstruction(int n_iter,string file):
+	n_iter(n_iter)
+	{	
+		
+		ibstream in(file);
+		
+				typedef uint32_t file_int;
+		typedef uint16_t file_half;
+		
+	  
+		//ibstream in("pet10k_full.bin");
+		
+		file_int in_magic;
+		
+		in >> in_magic;
+		
+		in >> n_pixels;
+		in >> emissions;
+		in >> n_tubes;	
+		
+		printf("EM: %d\n",emissions);
+		
+		scale = (float*)malloc(n_pixels*n_pixels*sizeof(float));
+		memset(scale,0.1f,sizeof(scale));
+		
+		printf("%f %f %f\n",scale[0],scale[1],scale[2]);
+
+		std::list<hits_per_pixel> pixels;
+		
+		
+		
+		for(;;){
+	  
+			if (in.eof()) break;
+			
+			file_half a, b;
+			in >> a >> b;
+
+			file_int count;
+
+			in >> count;
+				
+				int sum = 0;
+				
+				//cout << "blabla" << endl;
+				for(int i = 0;i < count; ++i){
+					
+					file_half x, y;
+					file_int hits;
+		
+					in >> x >> y >> hits;
+					pixel_location pixel(x,y);
+					hits_per_pixel data;
+					data.location = pixel;
+					data.probability = static_cast<float>(hits/static_cast<float>(emissions));
+					
+					scale[(x*n_pixels) + y] += data.probability;  
+				
+					pixels.push_back(data);
+			  
+					sum+=hits;
+	
+				}
+				
+			n.push_back(sum);
+			
+			system_matrix.push_back(pixels);
+			pixels.clear();
+	
+		}
+		
+		printf("Pixels: %d\nEmissions: %d\nTubes: %d\nLors: %d\n",n_pixels,emissions,n_tubes,(int)system_matrix.size());
+	
+	}
+
+	~Pet_reconstruction(){}
+
+	std::vector<float> emt(){
+		
+		cout << n_tubes << " " << n_pixels << endl; 	
+		
+		float y[n_pixels * n_pixels];
+		float u[n_lors];
+		std::vector<float> rho(n_pixels * n_pixels,0.5);
+		
+		std::vector< std::list<hits_per_pixel> >::iterator it;
+		std::list<hits_per_pixel>::iterator lt;
+		
+		for(int i = 0; i < n_iter;++i){
+		
+			for(int p = 0; p < n_pixels * n_pixels;++p){
+
+				y[p] = 0.0;
+			
+			}
+
+				int t = 0;
+				
+				for(it=system_matrix.begin(); it != system_matrix.end(); ++it){
+					
+					u[t] = 0.f;
+					
+					for(lt = (*it).begin(); lt != (*it).end();++lt){
+					
+						u[t] += (rho[LOCATION((*lt).location.first,(*lt).location.second,n_pixels)] * (*lt).probability) ;
+
+					}
+
+					t++;
+									
+				}
+				
+				t = 0;
+	
+				for(it=system_matrix.begin(); it != system_matrix.end(); ++it){
+					
+					
+					float phi = n[t]/u[t];
+					
+					for(lt = (*it).begin(); lt != (*it).end();++lt){
+					
+						y[LOCATION((*lt).location.first,(*lt).location.second,n_pixels)] += phi * (*lt).probability;
+						
+					}
+					t++;
+									
+				}
+	
+			for(int p = 0; p < n_pixels * n_pixels; ++p){
+									
+					rho[p]=(rho[p])*(y[p]);
+			}
+		}
+		
+		return rho;
+		
+	}
+	
+	int get_n_pixels(){return n_pixels;}
+	
+	
+};
+
+
--- src/pet_reconstruction.cpp
+++ src/pet_reconstruction.cpp
@@ -9,29 +9,52 @@
 //   by Linda Kaufman
 
 #include <cmdline.h>
+#include "bstream.h"
+#include "svg_ostream.h"
 #include "cmdline_types.h"
+#include "pet_rec.h"
 
 #if _OPENMP
 #include <omp.h>
 #endif
 
+std::vector< std::list<hits_per_pixel> > Pet_reconstruction::system_matrix;
+std::vector<float> Pet_reconstruction::n;
+
 int main(int argc, char *argv[]) {
+	
+	try {
+	  cmdline::parser cl;
 
-try {
-  cmdline::parser cl;
-  cl.footer("matrix_file measurements_file");
+	#if _OPENMP
+	  cl.add<size_t>     ("n-threads",'t', "number of OpenMP threads",          false);
+	#endif
+	  cl.add<std::string>("bin",'f', "system matrix binary file", false,"pet10k_full.bin");
+	  cl.add("iterations",'n', "number of iterations", false,10);
+	  
+	  cl.parse_check(argc, argv);
 
-#if _OPENMP
-  cl.add<size_t>     ("n-threads",   't', "number of OpenMP threads",          false);
-#endif
-
-  cl.parse_check(argc, argv);
-
-#if _OPENMP
-  if (cl.exist("n-threads")) {
-    omp_set_num_threads(cl.get<size_t>("n-threads"));
-  }
-#endif
+	#if _OPENMP
+	  if (cl.exist("n-threads")) {
+		omp_set_num_threads(cl.get<size_t>("n-threads"));
+	  }
+	#endif
+	
+	Pet_reconstruction *pet_r = new Pet_reconstruction(cl.get<int>("iterations"),cl.get<std::string>("bin"));
+	
+	std::vector<float> output(pet_r->get_n_pixels() * pet_r->get_n_pixels(),0.0);
+	
+	output = pet_r->emt();
+	
+	for(std::vector<float>::iterator it = output.begin(); it != output.end();++it){
+	
+	
+	printf("%f\n",*it);
+	
+	
+	}
+	
+	delete pet_r;
 
   // FIXME: IMPLEMENT ME!
 
