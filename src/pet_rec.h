3039b6c134c094aaad0856a5bd231d8118a8f774
Adam Strzelecki <ono@java.pl>
2012-11-27 12:58:35 +0100

    Cleanup formatting
    Please use 2 spaces for indention and trim trailing spaces on save!

--- src/pet_rec.h
+++ src/pet_rec.h
@@ -4,16 +4,16 @@
 #include <vector>
 #include <list>
 
-#define LOCATION(x,y,size)  (x*size) +y 
+#define LOCATION(x,y,size)  (x*size) +y
 
 typedef std::pair<size_t, size_t> pixel_location;
 typedef std::pair<size_t, size_t> lor;
 typedef uint32_t file_int;
 typedef uint16_t file_half;
 
- 
+
  #define fourcc(a, b, c, d) (((d)<<24) | ((c)<<16) | ((b)<<8) | (a))
- 
+
 // binary serialization                                  // n_pixels  n_detectors  triagular
 static const file_int magic_1 = fourcc('P','E','T','t'); //                           X
 static const file_int magic_2 = fourcc('P','E','T','s'); //     X                     X
@@ -25,15 +25,15 @@ using namespace std;
 
 struct hits_per_pixel{
 
-	pixel_location location;
-	float probability;
+  pixel_location location;
+  float probability;
 
 };
 
 struct mean_per_lor{
 
-	lor Lor;
-	int n;
+  lor Lor;
+  int n;
 
 };
 
@@ -41,213 +41,213 @@ struct mean_per_lor{
 class Pet_reconstruction{
 
 public:
-	Pet_reconstruction(int n_iter,string matrix,string mean):
-	n_iter(n_iter)
-	{	
+  Pet_reconstruction(int n_iter,string matrix,string mean):
+  n_iter(n_iter)
+  {
 
-		ibstream in(matrix);
-		
-		typedef uint32_t file_int;
-		typedef uint16_t file_half;
-		
-		file_int in_magic;
-		
-		in >> in_magic;	
-		in >> n_pixels;
-		in >> emissions;
-		in >> n_tubes;	
-		
-		scale = (float*)malloc(n_pixels*n_pixels*sizeof(float));
-		for(int p = 0; p < n_pixels * n_pixels; ++p){
+    ibstream in(matrix);
 
-			scale[p] = 0.f;
+    typedef uint32_t file_int;
+    typedef uint16_t file_half;
 
-		}
-		
-		std::ifstream mean_file(mean);
-		std::vector<mean_per_lor> lor_mean;
-		
-		
-		for(;;)
-		{
-		
-			int x,y,value;
-			
-			if (mean_file.eof()) break;
-				
-			mean_file >> y >> x >> value;
-			
-			mean_per_lor temp_obj;
-			
-			lor tmp(x,y);
-			
-			temp_obj.Lor = tmp ;
-			temp_obj.n = value;
-			
-			lor_mean.push_back(temp_obj);
-		
-		}
-		
+    file_int in_magic;
 
-		std::vector<hits_per_pixel> pixels;
-		
-		int index = 0;
-		
-		for(;;)
-		{
-	  
-			if (in.eof()) break;
-			
-			file_half a, b;
-			in >> a >> b;
-			lor _lor(a,b);
-			
-			n.push_back(get_mean_per_lor(a,b,lor_mean));
-			
-			//if(get_mean_per_lor(a,b,lor_mean) != 0){printf("get_mean: %d\n",get_mean_per_lor(a,b,lor_mean));}
+    in >> in_magic;
+    in >> n_pixels;
+    in >> emissions;
+    in >> n_tubes;
 
-			file_int count;
+    scale = (float*)malloc(n_pixels*n_pixels*sizeof(float));
+    for(int p = 0; p < n_pixels * n_pixels; ++p){
 
-			in >> count;
+      scale[p] = 0.f;
 
-				//cout << "blabla" << endl;
-				for(int i = 0;i < count; ++i)
-				{
-					
-					file_half x, y;
-					file_int hits;
-		
-					in >> x >> y >> hits;
-					pixel_location pixel(x,y);
-					hits_per_pixel data;
-					data.location = pixel;
-					data.probability = static_cast<float>(hits/static_cast<float>(emissions));
-					
-					scale[LOCATION(x,y,n_pixels)] += data.probability;  
-				
-					pixels.push_back(data);
+    }
 
-				}
-				
-			system_matrix.push_back(pixels);
-			pixels.clear();
-			index++;
-		}
+    std::ifstream mean_file(mean);
+    std::vector<mean_per_lor> lor_mean;
 
-		printf("Pixels: %d\nEmissions: %d\nTubes: %d\nLors: %d\n",n_pixels,emissions,n_tubes,(int)system_matrix.size());
-		printf("%d\n",system_matrix.size());
 
-	}
+    for(;;)
+    {
 
-	~Pet_reconstruction(){free(scale);}
-	
-	std::vector<float> emt(){
-	
-		float y[n_pixels * n_pixels];
-		std::vector<float> u(system_matrix.size(),0.f);
-		std::vector<float> rho(n_pixels * n_pixels,1.0f);
-		
-		for(int i = 0; i < n_iter;++i)
-		{	
-			if(i%10){
-				printf("%d\n",i);
-			}
-			
-			for(int p = 0; p < n_pixels * n_pixels;++p)
-			{
+      int x,y,value;
 
-				y[p] = 0.f;
-			
-			}
+      if (mean_file.eof()) break;
 
-			vector< vector<hits_per_pixel> >::iterator it_vector;
-			vector<hits_per_pixel>::iterator it_list;
+      mean_file >> y >> x >> value;
 
-			 int t = 0;
-			 
+      mean_per_lor temp_obj;
+
+      lor tmp(x,y);
+
+      temp_obj.Lor = tmp ;
+      temp_obj.n = value;
+
+      lor_mean.push_back(temp_obj);
+
+    }
+
+
+    std::vector<hits_per_pixel> pixels;
+
+    int index = 0;
+
+    for(;;)
+    {
+
+      if (in.eof()) break;
+
+      file_half a, b;
+      in >> a >> b;
+      lor _lor(a,b);
+
+      n.push_back(get_mean_per_lor(a,b,lor_mean));
+
+      //if(get_mean_per_lor(a,b,lor_mean) != 0){printf("get_mean: %d\n",get_mean_per_lor(a,b,lor_mean));}
+
+      file_int count;
+
+      in >> count;
+
+        //cout << "blabla" << endl;
+        for(int i = 0;i < count; ++i)
+        {
+
+          file_half x, y;
+          file_int hits;
+
+          in >> x >> y >> hits;
+          pixel_location pixel(x,y);
+          hits_per_pixel data;
+          data.location = pixel;
+          data.probability = static_cast<float>(hits/static_cast<float>(emissions));
+
+          scale[LOCATION(x,y,n_pixels)] += data.probability;
+
+          pixels.push_back(data);
+
+        }
+
+      system_matrix.push_back(pixels);
+      pixels.clear();
+      index++;
+    }
+
+    printf("Pixels: %d\nEmissions: %d\nTubes: %d\nLors: %d\n",n_pixels,emissions,n_tubes,(int)system_matrix.size());
+    printf("%d\n",system_matrix.size());
+
+  }
+
+  ~Pet_reconstruction(){free(scale);}
+
+  std::vector<float> emt(){
+
+    float y[n_pixels * n_pixels];
+    std::vector<float> u(system_matrix.size(),0.f);
+    std::vector<float> rho(n_pixels * n_pixels,1.0f);
+
+    for(int i = 0; i < n_iter;++i)
+    {
+      if(i%10){
+        printf("%d\n",i);
+      }
+
+      for(int p = 0; p < n_pixels * n_pixels;++p)
+      {
+
+        y[p] = 0.f;
+
+      }
+
+      vector< vector<hits_per_pixel> >::iterator it_vector;
+      vector<hits_per_pixel>::iterator it_list;
+
+       int t = 0;
+
                 for(it_vector = system_matrix.begin(); it_vector != system_matrix.end(); it_vector++)
-					{ 
-						u[t] = 0.0f;
-						
+          {
+            u[t] = 0.0f;
+
                         for(it_list = (*it_vector).begin(); it_list != (*it_vector).end(); it_list++)
                         {
 
-							u[t] += rho[LOCATION((*it_list).location.first,(*it_list).location.second,n_pixels)] * (*it_list).probability ;
+              u[t] += rho[LOCATION((*it_list).location.first,(*it_list).location.second,n_pixels)] * (*it_list).probability ;
 
                         }
-						t++;
-							
+            t++;
+
                     }
 
-				t = 0;
-				
-				for(it_vector=system_matrix.begin(); it_vector != system_matrix.end(); ++it_vector)
-				{
-				
-					if(n[t]> 0){
-						
-						float phi = n[t]/u[t];// n[t]/u[t];				
-						
-						
-						for(it_list = (*it_vector).begin(); it_list != (*it_vector).end();++it_list)
-						{
-							
-								y[LOCATION((*it_list).location.first,(*it_list).location.second,n_pixels)] += phi * (*it_list).probability;
-											
-						}
-					
-					}
-					t++;
-									
-				}
-					
-				for(int p = 0; p < n_pixels * n_pixels; ++p)
-				{				
-				
-								if(scale[p] > 0){
-								rho[p] *= (y[p]/scale[p]) ;
-								}
-								 //rho[LOCATION(x,p,n_pixels)] = rho[LOCATION(x,p,n_pixels)] *y[LOCATION(x,p,n_pixels)]; 
-				}
-		
-		}
-		
-		return rho;
-		
-	}
-	
-	int get_n_pixels(){return n_pixels;}
-	
+        t = 0;
+
+        for(it_vector=system_matrix.begin(); it_vector != system_matrix.end(); ++it_vector)
+        {
+
+          if(n[t]> 0){
+
+            float phi = n[t]/u[t];// n[t]/u[t];
+
+
+            for(it_list = (*it_vector).begin(); it_list != (*it_vector).end();++it_list)
+            {
+
+                y[LOCATION((*it_list).location.first,(*it_list).location.second,n_pixels)] += phi * (*it_list).probability;
+
+            }
+
+          }
+          t++;
+
+        }
+
+        for(int p = 0; p < n_pixels * n_pixels; ++p)
+        {
+
+                if(scale[p] > 0){
+                rho[p] *= (y[p]/scale[p]) ;
+                }
+                 //rho[LOCATION(x,p,n_pixels)] = rho[LOCATION(x,p,n_pixels)] *y[LOCATION(x,p,n_pixels)];
+        }
+
+    }
+
+    return rho;
+
+  }
+
+  int get_n_pixels(){return n_pixels;}
+
 private:
 
-	int n_tubes;
-	int n_pixels;
-	int n_iter;
-	int emissions;
-	int n_lors;
-	static std::vector< std::vector<hits_per_pixel> > system_matrix;
-	static std::vector<int> list_of_lors;
-	static std::vector<int> n;
-	float *scale;
-	
-	
-	int get_mean_per_lor(file_half &a,file_half &b,std::vector<mean_per_lor> &mean){
-		
-		std::vector<mean_per_lor>::iterator it;
-		
-		for(it = mean.begin(); it != mean.end(); ++it )	
-		{
-			
-			if(a == (*it).Lor.first && b == (*it).Lor.second)
-			{ 	
-				printf("Equal: LOR(%d,%d)\n",(*it).Lor.first,(*it).Lor.second);
-				return (*it).n;
-			}
-		}
-			
-		return 0.f;
-	}
-	
-};	
+  int n_tubes;
+  int n_pixels;
+  int n_iter;
+  int emissions;
+  int n_lors;
+  static std::vector< std::vector<hits_per_pixel> > system_matrix;
+  static std::vector<int> list_of_lors;
+  static std::vector<int> n;
+  float *scale;
+
+
+  int get_mean_per_lor(file_half &a,file_half &b,std::vector<mean_per_lor> &mean){
+
+    std::vector<mean_per_lor>::iterator it;
+
+    for(it = mean.begin(); it != mean.end(); ++it )
+    {
+
+      if(a == (*it).Lor.first && b == (*it).Lor.second)
+      {
+        printf("Equal: LOR(%d,%d)\n",(*it).Lor.first,(*it).Lor.second);
+        return (*it).n;
+      }
+    }
+
+    return 0.f;
+  }
+
+};
 
 
--- src/pet_reconstruction.cpp
+++ src/pet_reconstruction.cpp
@@ -23,48 +23,48 @@ std::vector<int> Pet_reconstruction::list_of_lors;
 std::vector<int> Pet_reconstruction::n;
 
 int main(int argc, char *argv[]) {
-	
-	try {
-	  cmdline::parser cl;
 
-	#if _OPENMP
-	  cl.add<size_t>     ("n-threads",'t', "number of OpenMP threads",          false);
-	#endif
-	  cl.add<std::string>("bin",'f', "system matrix binary file", false,"pet10k_full.bin");//"M_p128_d468_x100.bin");
-	  cl.add<std::string>("mean",'m', "mean file", false,"n_kuba.txt");
-	  cl.add("iterations",'n', "number of iterations", false,10);
-	  
-	  cl.parse_check(argc, argv);
+try {
+  cmdline::parser cl;
 
-	#if _OPENMP
-	  if (cl.exist("n-threads")) {
-		omp_set_num_threads(cl.get<size_t>("n-threads"));
-	  }
-	#endif
-	
-	Pet_reconstruction *pet_r = new Pet_reconstruction(cl.get<int>("iterations"),cl.get<std::string>("bin"),cl.get<std::string>("mean"));
-	
-	std::vector<float> output(pet_r->get_n_pixels() * pet_r->get_n_pixels(),0.0);
-	
-	output = pet_r->emt();
-	
-	ofstream data("out.txt");
-	
-	int x = 0;
-	int y = 0;
-	
-	for(std::vector<float>::iterator it = output.begin(); it != output.end();++it)
-	{	
-		
-		if(y%128 == 0 && y != 0){x++; y = 0;}
-		
-		//printf("%d   %d   %f\n",x,y,*it);
-		data << x << " " << y << " " << *it << endl;
-		y++;
-		
-	}
-	
-	delete pet_r;
+#if _OPENMP
+  cl.add<size_t>     ("n-threads",'t', "number of OpenMP threads",          false);
+#endif
+  cl.add<std::string>("bin",'f', "system matrix binary file", false,"pet10k_full.bin");//"M_p128_d468_x100.bin");
+  cl.add<std::string>("mean",'m', "mean file", false,"n_kuba.txt");
+  cl.add("iterations",'n', "number of iterations", false,10);
+
+  cl.parse_check(argc, argv);
+
+#if _OPENMP
+  if (cl.exist("n-threads")) {
+    omp_set_num_threads(cl.get<size_t>("n-threads"));
+  }
+#endif
+
+  Pet_reconstruction *pet_r = new Pet_reconstruction(cl.get<int>("iterations"),cl.get<std::string>("bin"),cl.get<std::string>("mean"));
+
+  std::vector<float> output(pet_r->get_n_pixels() * pet_r->get_n_pixels(),0.0);
+
+  output = pet_r->emt();
+
+  ofstream data("out.txt");
+
+  int x = 0;
+  int y = 0;
+
+  for(std::vector<float>::iterator it = output.begin(); it != output.end();++it)
+  {
+
+    if(y%128 == 0 && y != 0){x++; y = 0;}
+
+    //printf("%d   %d   %f\n",x,y,*it);
+    data << x << " " << y << " " << *it << endl;
+    y++;
+
+  }
+
+  delete pet_r;
 
   // FIXME: IMPLEMENT ME!
 
