90c20ab8bf125fe52daa344636187f816acebaf5
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2012-12-04 13:32:38 +0100

    pet_reconstriction now can write "history" of iterations new features in imaging.nb to  display this history

--- src/pet_reconstruction.cpp
+++ src/pet_reconstruction.cpp
@@ -42,9 +42,25 @@ try {
   cl.add<cmdline::string>("system",    's', "system matrix file",       true);
   cl.add<cmdline::string>("mean",      'm', "mean file",                true);
   cl.add<size_t>         ("iterations",'n', "number of iterations",     false, 0);
+  cl.add<int>            ("i-blocks",'i',"number of iteration blocks", false,1);
   cl.add<cmdline::string>("output",    'o', "output reconstruction",    false);
 
   cl.parse_check(argc, argv);
+  std::string fn;
+  std::string fn_ext;
+  std::string fn_wo_ext;
+  bool do_output=cl.exist("output");
+
+  if (do_output) {
+
+    fn         = cl.get<cmdline::string>("output");
+    auto it_fn_sep  = fn.find_last_of("\\/");
+    auto it_fn_ext  = fn.find_last_of(".");
+    fn_wo_ext  = fn.substr(0, it_fn_ext != std::string::npos
+                                && (it_fn_sep == std::string::npos || it_fn_sep < it_fn_ext)
+                                ? it_fn_ext : std::string::npos);
+    fn_ext     = fn.substr(it_fn_ext != std::string::npos?it_fn_ext:fn.size(),fn.size());
+  }
 
 #if _OPENMP
   if (cl.exist("n-threads")) {
@@ -52,6 +68,7 @@ try {
   }
 #endif
 
+  int n_i_blocks=cl.get<int>("i-blocks");
   reconstruction<> reconstructor(
     cl.get<size_t>("iterations"),
     cl.get<cmdline::string>("system"),
@@ -61,27 +78,27 @@ try {
   auto total_n_pixels=n_pixels*n_pixels;
   reconstruction<>::output_type rho(total_n_pixels, 0.0);
   reconstruction<>::output_type rho_detected(total_n_pixels, 0.0);
-  reconstructor.emt(cl.get<size_t>("iterations"));
-  rho = reconstructor.rho();
-  rho_detected = reconstructor.rho_detected();
 
-  if (!cl.exist("output")) return 0;
-
-  auto fn         = cl.get<cmdline::string>("output");
-  auto it_fn_sep     = fn.find_last_of("\\/");
-  auto it_fn_ext     = fn.find_last_of(".");
-  auto fn_wo_ext  = fn.substr(0, it_fn_ext != std::string::npos
-                             && (it_fn_sep == std::string::npos || it_fn_sep < it_fn_ext)
-                               ? it_fn_ext : std::string::npos);
-  auto fn_ext     = fn.substr(it_fn_ext != std::string::npos?it_fn_ext:fn.size(),fn.size());
-
-  std::ofstream out(fn);
-  write_text_from_vector(out,rho.begin(),rho.end(),n_pixels);
+  std::ofstream out;
+  std::ofstream out_detected;
+  if(do_output) {
+    out.open(fn);
+    out_detected.open(fn_wo_ext+"_detected"+fn_ext);
+  }
+  for(int i=0;i<n_i_blocks;++i) {
+    reconstructor.emt(cl.get<size_t>("iterations"));
+    rho = reconstructor.rho();
+    rho_detected = reconstructor.rho_detected();
+    if(do_output) {
+      write_text_from_vector(out,rho.begin(),rho.end(),n_pixels);
+      write_text_from_vector(out_detected,rho_detected.begin(),rho_detected.end(),n_pixels);
+    }
+  }
   out.close();
-
-  std::ofstream out_detected(fn_wo_ext+"_detected"+fn_ext);
-  write_text_from_vector(out_detected,rho_detected.begin(),rho_detected.end(),n_pixels);
   out_detected.close();
+
+  if (!do_output) return 0;
+
 
 
   // output reconstruction PNG
