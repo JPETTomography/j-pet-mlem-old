cf50c68bcb10fcf2592cc93e911a0ae6097afecb
Piotr Bialas <pbialas@th.if.uj.edu.pl>
2013-01-03 23:59:09 +0100

    Starting to test the SimpleLor and PixelMajorSystemMatrix

--- src/pix_major_matrix.h
+++ src/pix_major_matrix.h
@@ -9,19 +9,35 @@
    That means that in the same time only few pixels are processed in parallel.
    On shiva for example the biggest machine has 24 cores.
    We can afford to alloc memory for lors in an uneficient way providing for
-   quick acces and reduce them after the simulations for this pixel is reduced.
-
+   quick acces and reduce them after the simulations for this pixel is finished.
 */
 
 
-template<typename LorType>
+template<typename LorType, typename F = double>
 class PixelMajorSystemMatrix {
 
 public:
   typedef std::pair<LorType,int> HitType;
 
+
+  PixelMajorSystemMatrix(detector_ring<F> dr_a,
+                         int n_pixels_a,
+                         F pixel_size_a)
+    :n_lors_(SimpleLor::n_lors()),
+     n_pixels_(n_pixels_a),
+     n_pixels_half_(n_pixels_/2),
+     total_n_pixels_(n_pixels_half_*(n_pixels_half_+1)/2),
+     pixel_tmp_(total_n_pixels_,(int *)0),
+     pixel_(total_n_pixels_),
+     pixel_count_(total_n_pixels_),
+     index_to_lor_(SimpleLor::n_lors()) {};
+
   void add_to_t_matrix(const LorType &lor, int i_pixel) {
-    if(pixel_tmp_[i_pixel]==NULL) {
+
+    std::cerr<<"adding to  pixel"<<std::endl;
+
+    if(!pixel_tmp_[i_pixel]) {
+      std::cerr<<"alcating memory for "<<i_pixel<<" "<<n_lors_<<std::endl;
       pixel_tmp_[i_pixel]=new int[n_lors_]();
     }
 
@@ -29,32 +45,42 @@ public:
     pixel_count_[i_pixel]++;
   };
 
-  int t_get_element(lor_type lor, int i_pixel) {
-    std::binary_search(pixel_[i_pixel].begin(),
-                       pixel_[i_pixel].end(),
-                       make_pair(lor,0),
-                       HitTypeComparator());
+  ~PixelMajorSystemMatrix() {
+    for(int p=0;p<total_n_pixels_;++p) {
+      if(pixel_tmp_[p])
+        delete [] pixel_tmp_[p];
+    }
+  }
+
+  int t_get_element(LorType lor, int i_pixel) {
+    auto hit=std::lower_bound(pixel_[i_pixel].begin(),
+                              pixel_[i_pixel].end(),
+                              make_pair(lor,0),
+                              HitTypeComparator());
+
+    if(hit== pixel_[i_pixel].end())
+      return 0;
+    return hit.second;
   }
 
   void finalize_pixel(int i_pixel) {
     int count=0;
-    pixel_[i_pixel].resize(pixel_counts_[i_pixel]);
-    int it;
-    for(int lor=0;i<n_lors_;++lor) {
+    pixel_[i_pixel].resize(pixel_count_[i_pixel]);
+    int it=0;
+    for(int lor=0;lor<n_lors_;++lor) {
       int hits;
       if(hits=pixel_tmp_[i_pixel][lor]>0) {
-        pixel_[i_pixel][it]=std::make_pair<index_to_lor_[lor],hits>;
+        pixel_[i_pixel][it]=std::make_pair(index_to_lor_[lor],hits);
         it++;
       }
     }
-    delete [] pixel[i_pixel];
+    delete [] pixel_tmp_[i_pixel];
 
     std::sort(pixel_[i_pixel].begin(),
               pixel_[i_pixel].end(),
               LorType::less
               );
   }
-
 
   static size_t t_lor_index(const LorType &lor) {
     return LorType::t_index(lor);
@@ -63,13 +89,16 @@ public:
 private:
 
   struct HitTypeComparator {
-    bool operator()(const HitType &a,const HitType &) {
+    bool operator()(const HitType &a,const HitType &b) {
       return LorType::less(a.first,b.first);
     }
-  }
+  };
+
+  int n_pixels_;
   int n_pixels_half_;
+  int total_n_pixels_;
   int n_lors_;
-  std::vector<*int> pixel_tmp_;
+  std::vector<int*> pixel_tmp_;
   std::vector< std::vector<HitType> > pixel_;
   std::vector<int> pixel_count_;
   std::vector<LorType> index_to_lor_;
--- src/simple_lor.h
+++ src/simple_lor.h
@@ -1,31 +1,77 @@
 #pragma once
 
+#include<algorithm>
+
 class SimpleLor {
 public:
+
+ SimpleLor(int first_a = 0,int second_a = 0):first(first_a),second(second_a){};
+
+
   int first;
   int second;
 
   static int t_index(const SimpleLor &lor) {
     SimpleLor ordered_lor=lor;
     if(ordered_lor.first< ordered_lor.second)
-      swap(ordered_lor.first, ordered_lor.second);
+      std::swap(ordered_lor.first, ordered_lor.second);
     return (ordered_lor.first*(ordered_lor.first+1))/2+ordered_lor.second;
   }
 
   static void init(int n_detectors_a) {
     n_detectors_=n_detectors_a;
-    n_lors_     =(n_detectors*(n_detectors-1))/2;
+    n_lors_     =(n_detectors_*(n_detectors_-1))/2;
   }
+
+  static int n_lors() {return n_lors_;}
+
+  bool operator!=(const SimpleLor &rhs) {
+    return first!= rhs.first || second!=rhs.second;
+  }
+
+  struct Comparator { 
+     int operator()(const SimpleLor &a, const SimpleLor &b) {
+      if(a.first<b.first) return 1;
+      if(a.first>b.first) return 0;      return a.second<b.second;
+    }
+  };
+
+  class Iterator ;
+
+  static Iterator begin() ;
+  static Iterator end() ;
 
   static const Comparator less;
 private:
   static int n_lors_;
   static int n_detectors_;
-
-  struct Comparator {
-    int operator()(const SimpleLor &a, const SimpleLor &b) {
-      if(a.first<b.first) return 1;
-      if(a.first>b.first) return 0;      return a.second<b.second;
-    }
-  };
 };
+   
+
+class SimpleLor::Iterator {
+ public:
+ Iterator(int first, int second):lor(first,second){};
+
+    SimpleLor::Iterator &operator++() {
+      lor.second++;
+      if(lor.second>=lor.first){
+	lor.second=0;
+	lor.first++;
+      }
+      return *this;
+    }
+
+    SimpleLor &operator*() {
+      return lor;
+    }
+
+    bool operator!=(const SimpleLor::Iterator rhs) {
+      return lor!=rhs.lor;
+    }
+ private:
+    SimpleLor lor;
+  };
+
+
+inline SimpleLor::Iterator SimpleLor::begin() {return SimpleLor::Iterator(1,0);}
+inline SimpleLor::Iterator SimpleLor::end() {return SimpleLor::Iterator(n_detectors_,0);}
