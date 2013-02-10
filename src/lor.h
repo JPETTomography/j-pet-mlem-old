ae79de29a2e9df9a62d780723d3e33d3bb11af5e
Adam Strzelecki <ono@java.pl>
2013-02-10 22:57:09 +0100

    Add symmetry calculation

--- src/lor.h
+++ src/lor.h
@@ -36,4 +36,29 @@ template <typename SType = int> class LOR : public std::pair<SType, SType> {
     // FIXME: implement me!
     throw(__PRETTY_FUNCTION__);
   }
+
+  /// Computes LOR based on given symmetry (1 out 8)
+  /// @param symmetry    number (0..7)
+  /// @param n_detectors number of detectors
+  LOR symmetric(S symmetry, S n_detectors) const {
+    S n_2_detectors = 2 * n_detectors;
+    S n_detectors_2 = n_detectors / 2;
+    S n_detectors_4 = n_detectors / 4;
+    S n_1_detectors_2 = n_detectors + n_detectors_2;
+    S n_1_detectors_4 = n_detectors + n_detectors_4;
+    LOR lor(*this);
+    if (symmetry & 1) {
+      lor.first = (n_2_detectors - lor.first) % n_detectors;
+      lor.second = (n_2_detectors - lor.second) % n_detectors;
+    }
+    if (symmetry & 2) {
+      lor.first = (n_1_detectors_2 - lor.first) % n_detectors;
+      lor.second = (n_1_detectors_2 - lor.second) % n_detectors;
+    }
+    if (symmetry & 4) {
+      lor.first = (n_1_detectors_4 - lor.first) % n_detectors;
+      lor.second = (n_1_detectors_4 - lor.second) % n_detectors;
+    }
+    return lor;
+  }
 };
