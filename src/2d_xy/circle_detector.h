9f96789707e0823248c4b696904adaee6cb83011
Adam Strzelecki <ono@java.pl>
2013-11-13 18:47:43 +0100

    Fix ICC compilation

--- src/2d_xy/circle_detector.h
+++ src/2d_xy/circle_detector.h
@@ -6,20 +6,20 @@ template <typename FType = double> class CircleDetector {
  public:
   typedef FType F;
   typedef F Angle;
-  typedef ::Point Point;
+  typedef ::Point<FType> Point;
 
   CircleDetector(F radius) : radius(radius), center() {}
 
   CircleDetector(F radius, Point center) : radius(radius), center(center) {}
 
-  SquareDetector& rotate(Angle phi __attribute__((unused))) { return *this; }
+  CircleDetector& rotate(Angle phi __attribute__((unused))) { return *this; }
 
-  SquareDetector rotated(Angle phi __attribute__((unused))) {
+  CircleDetector rotated(Angle phi __attribute__((unused))) {
     SquareDetector r(radius, center);
     return r;
   }
 
-  SquareDetector& operator+=(Point t) {
+  CircleDetector& operator+=(Point t) {
     center += t;
     return *this;
   }
