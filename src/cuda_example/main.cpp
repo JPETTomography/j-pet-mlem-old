f42bec9248f3c8cd0c88584a06c3532006bd723e
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2013-11-20 21:59:52 +0100

    Print CUDA example results

--- src/cuda_example/main.cpp
+++ src/cuda_example/main.cpp
@@ -1,8 +1,7 @@
-void run_kernel(char* str, int* val, int str_size, int val_size);
+#include <cstdio>
 
-#if !HAVE_CUDA
-#error Need to have CUDA!
-#endif
+// reference stuff from kernel.cu file
+void run_kernel(char* str, int* val, int str_size, int val_size);
 
 int main(int argc __attribute__((unused)),
          char* argv[] __attribute__((unused))) {
@@ -10,6 +9,8 @@ int main(int argc __attribute__((unused)),
   char str[] = "Hello \0\0\0\0\0\0";
   int val[] = { 15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+  printf("before: %s\n", str);
   run_kernel(str, val, sizeof(str), sizeof(val));
+  printf("after: %s\n", str);
   return 0;
 }
