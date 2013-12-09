33ef785e31539a3317d07f44c8fea60de0384bc6
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2013-12-09 17:18:04 +0100

    Adds some info about purpose of hooks

--- src/util/cmdline_hooks.h
+++ src/util/cmdline_hooks.h
@@ -5,7 +5,10 @@
 
 namespace cmdline {
 
+/// loads serialized command line parameters from config file given as argument
 bool load(cmdline::parser& parser, string& value, const std::string& arg);
+
+/// skips setting option if it comes from config file
 bool not_from_file(cmdline::parser& parser, int& value, const std::string& arg);
 
 }  // cmdline
