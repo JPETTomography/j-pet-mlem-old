aab21495a23207e128e11fde3bf331437bb4e85f
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2013-12-09 21:27:11 +0100

    Nested configs relative path resolve

--- src/util/cmdline_hooks.cpp
+++ src/util/cmdline_hooks.cpp
@@ -5,28 +5,37 @@
 
 namespace cmdline {
 
-int loading = 0;
+std::vector<std::string> dir_stack;
 
 bool load(cmdline::parser& parser,
           string& value __attribute__((unused)),
           const std::string& arg) {
-  std::ifstream in(arg);
+  std::string path = arg;
+  if (!dir_stack.empty() && path.substr(0, 1) != "/") {
+    path = dir_stack.back() + path;
+  }
   if (parser.exist("verbose")) {
-    std::cout << "load: " << arg << std::endl;
+    std::cout << "load: " << path << std::endl;
   }
+  std::ifstream in(path);
   if (!in.is_open()) {
-    throw("cannot open input config file: " + arg);
+    throw("cannot open input config file: " + path);
   }
-  ++loading;
-  parser.try_parse(in, false, arg.c_str());
-  --loading;
+  auto dir_sep = path.find_last_of('/');
+  if (dir_sep != std::string::npos) {
+    dir_stack.push_back(arg.substr(0, dir_sep + 1));
+  } else {
+    dir_stack.push_back(std::string());
+  }
+  parser.try_parse(in, false, path.c_str());
+  dir_stack.pop_back();
   return true;
 }
 
 bool not_from_file(cmdline::parser& parser __attribute__((unused)),
                    int& value __attribute__((unused)),
                    const std::string& arg __attribute__((unused))) {
-  return loading == 0;
+  return dir_stack.empty();
 }
 
 }  // cmdline
