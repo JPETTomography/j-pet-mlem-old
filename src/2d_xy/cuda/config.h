10e10166cb4ea30a3a4f028db4949d6c2ba9ddb9
Jakub Kowal <jakub.kowal@uj.edu.pl>
2014-01-14 09:18:23 +0100

    deposition deoth added to geometry_methods.cuh gpu code

--- src/2d_xy/cuda/config.h
+++ src/2d_xy/cuda/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // FIXME: this must be dynamic!
-#define NUMBER_OF_DETECTORS 64
+#define NUMBER_OF_DETECTORS 204
 #define LORS (NUMBER_OF_DETECTORS*(NUMBER_OF_DETECTORS + 1)) / 2
 
 #define TP 6.28318530f
