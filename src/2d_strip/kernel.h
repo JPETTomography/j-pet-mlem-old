b46241b8573dfeec06bf1d3da480f399bc2600b3
Adam Strzelecki <ono@java.pl>
2014-10-17 20:05:33 +0200

    Strip kernel: Optimize operations

--- src/2d_strip/kernel.h
+++ src/2d_strip/kernel.h
@@ -14,15 +14,16 @@ template <typename FType = double> class Kernel {
  private:
   const F inv_pow_sigma_z;
   const F inv_pow_sigma_dl;
-  const F sqrt_det_cor_mat;
+  const F inv_pow_two_pi_sqrt_det_cor_mat;
 
  public:
   _ Kernel(F sigma_z, F sigma_dl)
       : inv_pow_sigma_z(1 / (sigma_z * sigma_z)),
         inv_pow_sigma_dl(1 / (sigma_dl * sigma_dl)),
-        sqrt_det_cor_mat(inv_pow_sigma_z *  //
-                         inv_pow_sigma_z *  //
-                         inv_pow_sigma_dl) {}
+        inv_pow_two_pi_sqrt_det_cor_mat(F(1 / (2 * M_PI * M_PI)) *  //
+                                        inv_pow_sigma_z *           //
+                                        inv_pow_sigma_z *           //
+                                        inv_pow_sigma_dl) {}
 
   _ F test(const F y,
            const F z,
@@ -47,31 +48,26 @@ template <typename FType = double> class Kernel {
     F sec = 1 / cos;
     F sec_sq = sec * sec;
 
-    FVec vec_o = { -(pixel_center.y + y - R) * tan * sec_sq,
-                   -(pixel_center.y + y + R) * tan * sec_sq,
-                   -(pixel_center.y + y) * sec * (1 + 2 * (tan * tan)) };
-    FVec vec_a = { -(pixel_center.y + y - R) * sec_sq,
-                   -(pixel_center.y + y + R) * sec_sq,
-                   -2 * (pixel_center.y + y) * (sec * tan) };
-    FVec vec_b = { pixel_center.x - (pixel_center.y * tan),
-                   pixel_center.x - (pixel_center.y * tan),
-                   -2 * pixel_center.y * sec };
+    F pixel_center_y_p_y = pixel_center.y + y;
+    FVec vec_a = { -(pixel_center_y_p_y - R) * sec_sq,
+                   -(pixel_center_y_p_y + R) * sec_sq,
+                   -2 * pixel_center_y_p_y * sec * tan };
+    FVec vec_o = { vec_a.p * tan,
+                   vec_a.q * tan,
+                   -pixel_center_y_p_y * sec * (1 + 2 * tan * tan) };
+    F vec_b_pq = pixel_center.x - pixel_center.y * tan;
+    FVec vec_b = { vec_b_pq, vec_b_pq, -2 * pixel_center.y * sec };
 
     F a_ic_a = multiply_inv_cor_mat(vec_a, vec_a);
     F b_ic_a = multiply_inv_cor_mat(vec_b, vec_a);
     F b_ic_b = multiply_inv_cor_mat(vec_b, vec_b);
     F o_ic_b = multiply_inv_cor_mat(vec_o, vec_b);
 
-    F norm = a_ic_a + (2 * o_ic_b);
+    F norm = a_ic_a + 2 * o_ic_b;
 
-    const F INV_POW_TWO_PI = F(1 / (2 * M_PI * M_PI));
-
-    F element_before_exp =
-        INV_POW_TWO_PI * (sqrt_det_cor_mat / compat::sqrt(norm));
-
-    F exp_element = -F(0.5) * (b_ic_b - ((b_ic_a * b_ic_a) / norm));
-
-    F exp = compat::exp(exp_element);
+    F element_before_exp = inv_pow_two_pi_sqrt_det_cor_mat / compat::sqrt(norm);
+    F exp_arg = -F(0.5) * (b_ic_b - b_ic_a * b_ic_a / norm);
+    F exp = compat::exp(exp_arg);
 
     return element_before_exp * exp;
   }
