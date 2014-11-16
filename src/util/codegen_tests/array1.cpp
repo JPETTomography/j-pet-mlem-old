93f84c03f303bd43edbc539ecdf40276b5185a14
Adam Strzelecki <ono@java.pl>
2014-11-16 18:54:31 +0100

    Use codegen test to cover CUDA

--- src/util/codegen_tests/array1.cpp
+++ src/util/codegen_tests/array1.cpp
@@ -1,21 +1,39 @@
-// run: c++ -O3 -fomit-frame-pointer -std=c++11 -c array1.cpp
-// run: otool -tv array1.o
-// expected:
-//   array1.o:
-//   (__TEXT,__text) section
-//   __Z3addii:
-//   0000000000000000	addl	%esi, %edi
-//   0000000000000002	movl	%edi, %eax
-//   0000000000000004	retq
+//  c++ -O2 -fomit-frame-pointer -std=c++11 -c -S -o - array1.cpp | c++filt
+//
+//  add(int, int):
+//  	addl	%esi, %edi
+//  	movl	%edi, %eax
+//  	retq
+//
+//  nvcc -std=c++11 -x cu -ptx -o - array1.cpp | sed s,_param_,:, | c++filt -n
+//
+//  .visible .func  (.param .b32 func_retval0) add(int, int)(
+//  	.param .b32 add(int, int):0,
+//  	.param .b32 add(int, int):1
+//  )
+//  {
+//  	.reg .s32 	%r<4>;
+//
+//
+//  	ld.param.u32 	%r1, [add(int, int):0];
+//  	ld.param.u32 	%r2, [add(int, int):1];
+//  	add.s32 	%r3, %r2, %r1;
+//  	st.param.b32	[func_retval0+0], %r3;
+//  	ret;
+//  }
 
+#define _ __device__
+#if !__CUDACC__
+#define __device__
+#endif
 #include "../array.h"
 
 struct Foo {
   int foo;
-  Foo(int foo) : foo(foo) {}
+  _ Foo(int foo) : foo(foo) {}
 };
 
-int add(int a, int b) {
-  util::array<2, Foo> foos{ a, b };
+_ int add(int a, int b) {
+  util::array<4, Foo> foos{ a, b };
   return foos[0].foo + foos[1].foo;
 }
--- src/util/codegen_tests/array2.cpp
+++ src/util/codegen_tests/array2.cpp
@@ -1,20 +1,40 @@
-// run: c++ -O3 -fomit-frame-pointer -std=c++11 -c array2.cpp
-// run: otool -tv array2.o
-// expected:
-//   array2.o:
-//   (__TEXT,__text) section
-//   __Z3addff:
-//   0000000000000000	addss	%xmm1, %xmm0
-//   0000000000000004	retq
+//  c++ -O2 -fomit-frame-pointer -std=c++11 -c -S -o - array2.cpp | c++filt
+//
+//  add(float, float):
+//  	addss	%xmm1, %xmm0
+//  	retq
+//
+//  nvcc -std=c++11 -x cu -ptx -o - array2.cpp | sed s,_param_,:, | c++filt -n
+//
+//  THIS IS EXPECTED, BUT AS OF CUDA 7.0 EA DOES NOT WORK!
+//
+//  .visible .func  (.param .b32 func_retval0) add(int, int)(
+//  	.param .b32 add(int, int):0,
+//  	.param .b32 add(int, int):1
+//  )
+//  {
+//  	.reg .s32 	%r<4>;
+//
+//
+//  	ld.param.u32 	%r1, [add(int, int):0];
+//  	ld.param.u32 	%r2, [add(int, int):1];
+//  	add.s32 	%r3, %r2, %r1;
+//  	st.param.b32	[func_retval0+0], %r3;
+//  	ret;
+//  }
 
+#define _ __device__
+#if !__CUDACC__
+#define __device__
+#endif
 #include "../array.h"
 
 struct Bar {
   float foo, bar;
-  Bar(float foo, float bar) : foo(foo), bar(bar) {}
+  _ Bar(float foo, float bar) : foo(foo), bar(bar) {}
 };
 
-float add(float a, float b) {
+_ float add(float a, float b) {
   util::array<4, Bar> bars;
   Bar bar(a, b);
   bars.push_back(bar);
