ad2743c357e4d8b8781e8812da06e9f9f0cba802
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2014-11-20 15:57:27 +0100

    Barrel: CompoundDetector returns closest detector

--- src/2d/barrel/compound_detector.h
+++ src/2d/barrel/compound_detector.h
@@ -11,6 +11,8 @@
 #define MAX_DETECTORS 256
 #endif
 
+#include <iostream>  // DEBUG
+
 namespace PET2D {
 namespace Barrel {
 
@@ -31,12 +33,13 @@ class CompoundDetector : public util::array<MaxDetectors, DetectorType> {
   using Pixel = PET2D::Pixel<S>;
   using Point = PET2D::Point<F>;
   using Event = Barrel::Event<F>;
-  using Base = std::vector<Detector>;
+  using Base = util::array<MaxDetectors, Detector>;
   using CircleDetector = Barrel::CircleDetector<F>;
 
   /// Tries to detect given event.
 
   /// \return number of coincidences (detector hits)
+  /// \todo FIXME: Now returns first index to closest detector
   template <class RandomGenerator, class AcceptanceModel>
   short detect(RandomGenerator& gen,    ///< random number generator
                AcceptanceModel& model,  ///< acceptance model
@@ -44,16 +47,24 @@ class CompoundDetector : public util::array<MaxDetectors, DetectorType> {
                LOR& lor,                ///<[out] lor of the event
                F& position              ///<[out] position of the event
                ) {
+    (void)(gen);       // mark as used
+    (void)(model);     // mark as used
+    (void)(lor);       // mark as used
+    (void)(position);  // mark as used
     int close_indices[MaxDetectors];
     int n_close_indices = 0;
     for (int i = 0; i < c_detectors.size(); ++i) {
       auto& circle = c_detectors[i];
-      if (circle.intersections(e).size() == 2) {
+      if (circle.intersects(e)) {
         close_indices[n_close_indices++] = i;
       }
     }
-    (void)(gen, model, e, lor, position);
-    return 0;
+    std::sort(
+        &close_indices[0], &close_indices[n_close_indices], [&](int a, int b) {
+          return (e - c_detectors[close_indices[a]]).length2() <
+                 (e - c_detectors[close_indices[b]]).length2();
+        });
+    return close_indices[0];
   }
 
   friend util::svg_ostream<F>& operator<<(util::svg_ostream<F>& svg,
@@ -71,14 +82,14 @@ class CompoundDetector : public util::array<MaxDetectors, DetectorType> {
   }
 
   template <class... Args> void emplace_back(Args&&... args) {
-    Base::emplace_back(std::forward<Args&&>(args)...);
+    Base::emplace_back(std::forward<Args>(args)...);
     c_detectors.push_back(this->back().circumscribe_circle());
   }
 
   const CircleDetector& circumscribed(int i) { return c_detectors[i]; }
 
  private:
-  std::vector<CircleDetector> c_detectors;
+  util::array<MaxDetectors, CircleDetector> c_detectors;
 };
 }
 }
--- src/2d/barrel/compound_detector_test.cpp
+++ src/2d/barrel/compound_detector_test.cpp
@@ -16,15 +16,23 @@ TEST("2d/barrel/compound_detector/math") {
     detector.emplace_back(5., 1., 2., 2.);
     detector.emplace_back(5., 5., 2., 2.);
 
-    CHECK(Point<>(1., 1.) == detector.circumscribed(0).center);
-    CHECK(Point<>(1., 5.) == detector.circumscribed(1).center);
-    CHECK(Point<>(5., 1.) == detector.circumscribed(2).center);
-    CHECK(Point<>(5., 5.) == detector.circumscribed(3).center);
+    CHECK(Point<>(1., 1.) == detector.circumscribed(0));
+    CHECK(Point<>(1., 5.) == detector.circumscribed(1));
+    CHECK(Point<>(5., 1.) == detector.circumscribed(2));
+    CHECK(Point<>(5., 5.) == detector.circumscribed(3));
 
     CHECK(std::sqrt(2.) == detector.circumscribed(0).radius);
     CHECK(std::sqrt(2.) == detector.circumscribed(1).radius);
     CHECK(std::sqrt(2.) == detector.circumscribed(2).radius);
     CHECK(std::sqrt(2.) == detector.circumscribed(3).radius);
+
+    LOR<> lor;
+    double position;
+    Event<> e1(0, 0, M_PI_2);
+    CHECK(0 == detector.detect(e1, e1, e1, lor, position));
+
+    Event<> e2(4, 4, M_PI_2);
+    CHECK(2 == detector.detect(e2, e2, e2, lor, position));
   }
 
   SECTION("circle_detector") {
@@ -34,10 +42,10 @@ TEST("2d/barrel/compound_detector/math") {
     detector.emplace_back(2., Point<>(5., 1.));
     detector.emplace_back(2., Point<>(5., 5.));
 
-    CHECK(Point<>(1., 1.) == detector.circumscribed(0).center);
-    CHECK(Point<>(1., 5.) == detector.circumscribed(1).center);
-    CHECK(Point<>(5., 1.) == detector.circumscribed(2).center);
-    CHECK(Point<>(5., 5.) == detector.circumscribed(3).center);
+    CHECK(Point<>(1., 1.) == detector.circumscribed(0));
+    CHECK(Point<>(1., 5.) == detector.circumscribed(1));
+    CHECK(Point<>(5., 1.) == detector.circumscribed(2));
+    CHECK(Point<>(5., 5.) == detector.circumscribed(3));
 
     CHECK(2. == detector.circumscribed(0).radius);
     CHECK(2. == detector.circumscribed(1).radius);
