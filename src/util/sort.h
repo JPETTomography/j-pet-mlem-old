ca27c9f48a2f4c1415acfbad84e0d23220ebecdf
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2014-12-09 18:15:14 +0100

    util::heap_sort valid implementation with comments
    Comparator is less-than!

--- src/util/sort.h
+++ src/util/sort.h
@@ -4,18 +4,19 @@ namespace util {
 
 namespace {
 template <typename T, class Comparator>
-_ void heap_sort_sift_down(T* a, int start, int end, Comparator compare) {
-  int root = start;
-  while (root * 2 + 1 < end) {
-    int child = 2 * root + 1;
-    if (child + 1 < end && !compare(a[child], a[child + 1])) {
+_ void heap_sort_sift_down(T* a, int root, int count, Comparator compare) {
+  // traverse down
+  for (int child = 2 * root + 1; child < count; child = 2 * root + 1) {
+    // select largest child of two
+    if (child + 1 < count && compare(a[child], a[child + 1])) {
       ++child;
     }
-    if (!compare(a[root], a[child])) {
+    // if child is larger than parent, swap them
+    if (compare(a[root], a[child])) {
       compat::swap(a[child], a[root]);
       root = child;
     } else {
-      return;
+      break;
     }
   }
 }
@@ -25,12 +26,12 @@ template <typename T, class Comparator>
 _ void heap_sort(T* a, T* b, Comparator compare) {
   int count = static_cast<int>(b - a);
   // heapify
-  for (int start = (count - 2) / 2; start >= 0; --start) {
-    heap_sort_sift_down(a, start, count, compare);
+  for (int root = (count - 2) / 2; root >= 0; --root) {
+    heap_sort_sift_down(a, root, count, compare);
   }
   // sort
   for (int end = count - 1; end > 0; --end) {
-    compat::swap(a[end], a[0]);
+    compat::swap(a[end], a[0]);  // move largest value to the end
     heap_sort_sift_down(a, 0, end, compare);
   }
 }
