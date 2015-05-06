d0badb946a1962ff49d3206bce3f04eb500532d5
Piotr Bialas <pbialas7@gmail.com>
2015-05-06 18:16:43 +0200

    Started adding phantom Monte-Carlo.

---
+++ src/3d/longitudinal/phantom_monte_carlo.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include<random>
+
+template <typename Phantom, typename Detector> class PhantomMonteCarlo {
+public:
+    using F=Phantom::F;
+    using Event=Phantom::Event;
+    using RNG = Phantom::RNG;
+    using Response = Detector::Response;
+
+
+  PhantomMonteCarlo(Phantom& phantom, const Detector& detctor)
+      : phantom_(phantom) detector_(detector) {}
+
+
+  int generate(size_t n_emisions) {
+      for(size_t i=0;i<n_emisions;++i) {
+          auto event=phantom_.gen_event(rng);
+
+      }
+  }
+
+ private:
+  RNG rng;
+  Phantom& phantom_;
+  Detector detector_;
+};
