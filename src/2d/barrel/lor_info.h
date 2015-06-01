f858f83d95f5c90cc164b7a0faf7fa5c818ae33a
Piotr Bialas <pbialas7@gmail.com>
2015-06-01 22:46:16 +0200

    Looks that reading LorInfo from binary file is working.
    No grid info /read/written possible mistakes if different pixel data is
    used in generating LorInfo and Recosntruction:(

--- src/2d/barrel/lor_info.h
+++ src/2d/barrel/lor_info.h
@@ -22,28 +22,46 @@ template <typename FType, typename SType> class LorInfo {
   LorInfo(SType n_detectors, const PixelGrid& grid)
       : n_detectors(n_detectors),
         max_index((int(n_detectors - 1) * (n_detectors)) / 2 + n_detectors - 2),
-        lor_info_(max_index + 1),
-        grid(grid) {}
+        grid(grid),
+        lor_info_(max_index + 1) {}
 
   PixelInfoContainer& operator[](const LOR& lor) {
     return lor_info_[lor.index()];
   }
 
-  std::ifstream& read(std::ifstream& in) {
+  std::istream& read(std::istream& in) {
     while (read_lor_info(in))
       ;
     return in;
   }
 
-  std::ifstream& read_lor_info(std::ifstream& in) {
+  std::istream& read_lor_info(std::istream& in) {
     int lor_desc[3];
     in.read((char*)lor_desc, 3 * sizeof(int));
+
     if (in) {
       LOR lor(lor_desc[0], lor_desc[1]);
-      lor_info_[lor].reserve(lor_desc[2]);
-      in.read((char*)&lor_info_[lor][0], sizeof(PixelInfo) * lor_desc[2]);
+      lor_info_[lor.index()].resize(lor_desc[2]);
+      in.read((char*)&lor_info_[lor.index()][0],
+              sizeof(PixelInfo) * lor_desc[2]);
     }
     return in;
+  }
+
+  void print(std::ostream& out) {
+    for (int d1 = 0; d1 < n_detectors; ++d1) {
+      for (int d2 = 0; d2 < d1; ++d2) {
+        LOR lor(d1, d2);
+        auto index = lor.index();
+        if (lor_info_[index].size() > 0) {
+          out << d1 << " " << d2 << "\n";
+          for (PixelInfo& info : lor_info_[index]) {
+            out << info.pixel.x << " " << info.pixel.y << " " << info.t << " "
+                << info.distance << " " << info.fill << "\n";
+          }
+        }
+      }
+    }
   }
 
   const SType n_detectors;
--- src/3d/hybrid/reconstruction_cmd.cpp
+++ src/3d/hybrid/reconstruction_cmd.cpp
@@ -8,6 +8,7 @@
 #include "2d/barrel/ring_scanner.h"
 #include "2d/barrel/generic_scanner.h"
 #include "2d/barrel/scanner_builder.h"
+#include "2d/barrel/lor_info.h"
 
 using FType = float;
 using SType = int;
@@ -16,13 +17,49 @@ using Detector = PET2D::Barrel::SquareDetector<FType>;
 using Scanner2D = PET2D::Barrel::GenericScanner<Detector, 192, SType>;
 using Point = PET2D::Point<FType>;
 
-
 int main(int argc, char* argv[]) {
 
   cmdline::parser cl;
+  cl.add<std::string>("lor-info", '\0', "lor-pixel information", true);
   PET2D::Barrel::add_matrix_options(cl);
   try {
     cl.try_parse(argc, argv);
+
+    PET2D::Barrel::set_big_barrel_options(cl);
+    auto scanner =
+        PET2D::Barrel::ScannerBuilder<Scanner2D>::build_multiple_rings(
+            PET2D_BARREL_SCANNER_CL(cl, FType));
+
+    auto output = cl.get<cmdline::path>("output");
+    auto output_base_name = output.wo_ext();
+    auto ext = output.ext();
+
+
+    auto lor_info_file_name=cl.get<std::string>("lor-info");
+
+    std::ifstream lor_info_istream(lor_info_file_name, std::ios::binary);
+
+    FType pixel_size = 0.005;
+    if (cl.exist("s-pixel"))
+      pixel_size = cl.get<double>("s-pixel");
+    FType fov_radius = cl.get<double>("fov-radius");
+    std::cout << "fov " << fov_radius << " size " << pixel_size << "\n";
+    SType n_columns = 2 * SType(std::ceil(fov_radius / pixel_size));
+    SType n_rows = n_columns;
+    std::cout << "cols " << n_columns << " rows " << n_rows << "\n";
+    PET2D::PixelGrid<FType, SType> grid(
+        n_columns,
+        n_rows,
+        pixel_size,
+        Point(-pixel_size * n_columns / 2, -pixel_size * n_rows / 2));
+
+
+
+    PET2D::Barrel::LorInfo<FType, SType> lor_info(scanner.size(), grid);
+    lor_info.read(lor_info_istream);
+    lor_info.print(std::cout);
+
+
   } catch (cmdline::exception& ex) {
     if (ex.help()) {
       std::cerr << ex.usage();
@@ -40,14 +77,6 @@ int main(int argc, char* argv[]) {
   } catch (const char* ex) {
     std::cerr << "error: " << ex << std::endl;
   }
-
-  PET2D::Barrel::set_big_barrel_options(cl);
-  auto scanner = PET2D::Barrel::ScannerBuilder<Scanner2D>::build_multiple_rings(
-      PET2D_BARREL_SCANNER_CL(cl, FType));
-
-  auto output = cl.get<cmdline::path>("output");
-  auto output_base_name = output.wo_ext();
-  auto ext = output.ext();
 
   return 0;
 }
