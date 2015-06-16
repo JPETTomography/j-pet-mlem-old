ec004db9f2409b3c186d32c3f8354140adfe4430
Piotr Bialas <pbialas7@gmail.com>
2015-06-16 14:41:03 +0200

    Removed the symmetric_detector test from sparse matrix.
    This function was remowed.

--- src/2d/barrel/sparse_matrix_test.cpp
+++ src/2d/barrel/sparse_matrix_test.cpp
@@ -14,12 +14,5 @@ TEST("2d/barrel/sparse_matrix/symmetric_lor") {
                               int> matrix(128, 24, 0, 1);
 
   short detector = 1;
-  REQUIRE(matrix.symmetric_detector(detector, 0) == 1);
-  REQUIRE(matrix.symmetric_detector(detector, 1) == 23);
-  REQUIRE(matrix.symmetric_detector(detector, 2) == 11);
-  REQUIRE(matrix.symmetric_detector(detector, 3) == 13);
-  REQUIRE(matrix.symmetric_detector(detector, 4) == 5);
-  REQUIRE(matrix.symmetric_detector(detector, 5) == 7);
-  REQUIRE(matrix.symmetric_detector(detector, 6) == 19);
-  REQUIRE(matrix.symmetric_detector(detector, 7) == 17);
+
 }
