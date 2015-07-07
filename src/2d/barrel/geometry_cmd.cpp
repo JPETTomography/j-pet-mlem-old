95efd7e82ef2ded14362258d25dfe2b49c79a18b
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-07-07 17:31:01 +0200

    2d_barrel_geometry: Initial OpenMP implementation

--- src/2d/barrel/geometry_cmd.cpp
+++ src/2d/barrel/geometry_cmd.cpp
@@ -108,94 +108,114 @@ int main(int argc, char* argv[]) {
     lor_info_stream.write((const char*)&n_detectors, sizeof(S));
     grid.write(lor_info_stream);
 
-    // Loop over the LORs
-    int count = 0;
-    util::progress progress(verbose, n_detectors * (n_detectors + 1) / 2);
-    for (S d1 = 0; d1 < n_detectors; ++d1) {
-      for (S d2 = 0; d2 < d1; ++d2, ++count) {
-        progress(count);
+    std::vector<LOR> lor_map;
+    auto lor_count = LOR::end_for_detectors(n_detectors).index();
+    lor_map.resize(lor_count);
+    for (LOR lor(0, 0); lor < LOR::end_for_detectors(n_detectors); ++lor) {
+      lor_map[lor.index()] = lor;
+    }
 
-        boost::geometry::model::multi_polygon<Polygon> pair;
+    util::progress progress(verbose, lor_count);
 
-        boost::geometry::union_(detectors[d1], detectors[d2], pair);
-        Polygon lor;
-        boost::geometry::convex_hull(pair, lor);
-
-#if DEBUG
-        std::cout << "l: " << count << "  " << d1 << " " << d2 << "\n";
+#if _OPENMP && !_MSC_VER
+// We need to try catch inside OpenMP thread, otherwise we will not see the
+// error thrown.
+#define TRY try {
+#define CATCH                     \
+  }                               \
+  catch (std::string & ex) {      \
+    std::cerr << ex << std::endl; \
+    throw(ex);                    \
+  }
+#else
+#define TRY
+#define CATCH
 #endif
-        lor_info_stream.write((const char*)&d1, sizeof(d1));
-        lor_info_stream.write((const char*)&d2, sizeof(d2));
 
-        PET2D::LineSegment<F> segment(detectors_centers[d2],
-                                      detectors_centers[d1]);
-
-        // TODO: Calculate width of the LOR.
-        auto width1 = F(0);
-        auto width2 = F(0);
-        Detector detector1 = scanner[d1];
-        Detector detector2 = scanner[d2];
-        for (int i = 0; i < (int)detector1.size(); ++i) {
-          auto p1 = detector1[i];
-          auto dist1 = std::abs(segment.distance_from(p1));
-          if (dist1 > width1)
-            width1 = dist1;
-
-          auto p2 = detector2[i];
-          auto dist2 = std::abs(segment.distance_from(p2));
-          if (dist2 > width2)
-            width2 = dist2;
-        }
-        F width = width1 + width2;
-        lor_info_stream.write((const char*)&detectors_centers[d1],
-                              2 * sizeof(F));
-        lor_info_stream.write((const char*)&detectors_centers[d2],
-                              2 * sizeof(F));
-#if DEBUG
-        std::cout << detectors_centers[d1].x << " " << detectors_centers[d1].y
-                  << " "  //
-                  << detectors_centers[d2].x << " " << detectors_centers[d2].y
-                  << "\n";
+#if _OPENMP
+#pragma omp parallel for schedule(dynamic)
 #endif
-        lor_info_stream.write((const char*)&width, sizeof(F));
-        if (boost::geometry::intersects(lor, fov_circle)) {
-          for (int ix = 0; ix < grid.n_columns; ++ix)
-            for (int iy = 0; iy < grid.n_rows; ++iy) {
+    for (int lor_index = 0; lor_index < lor_count; ++lor_index) {
+      auto lor = lor_map[lor_index];
+      progress(lor_index);
+      TRY;
+      boost::geometry::model::multi_polygon<Polygon> pair;
 
-              Point center = grid.center_at(ix, iy);
-              Polygon pixel = BoostGeometryUtils::make_pixel(grid, ix, iy);
-              if (boost::geometry::intersects(pixel, fov_circle)) {
-                boost::geometry::model::multi_polygon<Polygon> inter;
-                boost::geometry::intersection(lor, pixel, inter);
-                auto area = boost::geometry::area(inter);
+      boost::geometry::union_(
+          detectors[lor.first], detectors[lor.second], pair);
+      Polygon lor_polygon;
+      boost::geometry::convex_hull(pair, lor_polygon);
 
-                if (area > 0) {
-                  auto pixel_area = boost::geometry::area(pixel);
-                  auto fill = area / pixel_area;
-                  auto t = segment.projection_scaled(center);
-                  auto distance = segment.distance_from(center);
-                  PixelInfo info;
-                  info.pixel = PET2D::Pixel<S>(ix, iy);
-                  info.t = t;
-                  info.distance = distance;
-                  info.fill = fill;
-                  lor_info.push_back_pixel_info(LOR(d1, d2), info);
-                }
+      PET2D::LineSegment<F> segment(detectors_centers[lor.second],
+                                    detectors_centers[lor.first]);
+
+      // TODO: Calculate width of the LOR.
+      auto width1 = F(0);
+      auto width2 = F(0);
+      Detector detector1 = scanner[lor.first];
+      Detector detector2 = scanner[lor.second];
+      for (int i = 0; i < (int)detector1.size(); ++i) {
+        auto p1 = detector1[i];
+        auto dist1 = std::abs(segment.distance_from(p1));
+        if (dist1 > width1)
+          width1 = dist1;
+
+        auto p2 = detector2[i];
+        auto dist2 = std::abs(segment.distance_from(p2));
+        if (dist2 > width2)
+          width2 = dist2;
+      }
+      F width = width1 + width2;
+
+      if (boost::geometry::intersects(lor_polygon, fov_circle)) {
+        for (int ix = 0; ix < grid.n_columns; ++ix)
+          for (int iy = 0; iy < grid.n_rows; ++iy) {
+
+            Point center = grid.center_at(ix, iy);
+            Polygon pixel = BoostGeometryUtils::make_pixel(grid, ix, iy);
+            if (boost::geometry::intersects(pixel, fov_circle)) {
+              boost::geometry::model::multi_polygon<Polygon> inter;
+              boost::geometry::intersection(lor_polygon, pixel, inter);
+              auto area = boost::geometry::area(inter);
+
+              if (area > 0) {
+                auto pixel_area = boost::geometry::area(pixel);
+                auto fill = area / pixel_area;
+                auto t = segment.projection_scaled(center);
+                auto distance = segment.distance_from(center);
+                PixelInfo info;
+                info.pixel = PET2D::Pixel<S>(ix, iy);
+                info.t = t;
+                info.distance = distance;
+                info.fill = fill;
+                lor_info.push_back_pixel_info(lor, info);
               }
             }
+          }
 
-          lor_info.sort();
-        }
+        lor_info.sort();
+      }
 
-        int n_pixels = lor_info[LOR(d1, d2)].pixels.size();
+      int n_pixels = lor_info[lor].pixels.size();
 
+#if _OPENMP
+#pragma omp critical
+#endif
+      {
+        lor_info_stream.write((const char*)&lor.first, sizeof(lor.first));
+        lor_info_stream.write((const char*)&lor.second, sizeof(lor.second));
+        lor_info_stream.write((const char*)&detectors_centers[lor.first],
+                              2 * sizeof(F));
+        lor_info_stream.write((const char*)&detectors_centers[lor.second],
+                              2 * sizeof(F));
+        lor_info_stream.write((const char*)&width, sizeof(F));
         lor_info_stream.write((const char*)&n_pixels, sizeof(int));
         if (n_pixels > 0)
-          lor_info_stream.write((const char*)&lor_info[LOR(d1, d2)].pixels[0],
+          lor_info_stream.write((const char*)&lor_info[lor].pixels[0],
                                 n_pixels * sizeof(PixelInfo));
-
-        progress(count, true);
       }
+      CATCH;
+      progress(lor_index, true);
     }
 
     return 0;
