c942d62668dd2b9d2a392d617268817341557a1c
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-10-10 13:52:33 +0200

    2D::LineSegment: Extend documentation

--- src/2d/geometry/line_segment.h
+++ src/2d/geometry/line_segment.h
@@ -12,7 +12,11 @@ namespace PET2D {
 /// Line segment
 
 /// Gives some extra functions allowing calculation Point distance to the
-/// segment.
+/// segment. The distance is calculated using normalized normal vector
+/// \f$ (A, B) \f$ and distance to origin \f$ C \f$ using formula:
+/// \f[
+///   A x + B y + C
+/// \f]
 template <typename FType> struct LineSegment {
   using F = FType;
   using Point = PET2D::Point<F>;
@@ -42,10 +46,12 @@ template <typename FType> struct LineSegment {
   }
 #endif
 
+  /// Returns distance from given Point to the line containing LineSegment.
   F distance_from(const Point& p) {
     return p.as_vector().dot(normal) - distance_from_origin;
   }
 
+  /// Returns Point projected to the line containing LineSegment.
   F projection(const Point& p) { return (p - start).dot(direction); }
   F projection_scaled(const Point& p) { return projection(p) / length; }
 
@@ -53,13 +59,14 @@ template <typename FType> struct LineSegment {
     return projection(p) - 0.5 * length;
   }
 
-  Point start;
-  Point end;
-  Point mid_point;
-  Vector direction;
-  Vector normal;
-  F length;
-  F distance_from_origin;
+  // FIXME: these should be const, but making them const breaks too much code
+  Point start;             ///< line segment 1st endpoint
+  Point end;               ///< line segment 2nd endpoint
+  Point mid_point;         ///< point lying exactly in the middle of the segment
+  Vector direction;        ///< direction vector from start towards end
+  Vector normal;           ///< vector normal to the direction
+  F length;                ///< length of the segment
+  F distance_from_origin;  ///< distance of the segment from the origin
 };
 
 }  // PET2D
