b8c925c1af795f4dd590765a0dbdf9fce8d4a19a
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-10-23 15:31:55 +0200

    Make Pixel/VoxelMap CUDA compatible & STL-free

--- src/2d/geometry/pixel_map.h
+++ src/2d/geometry/pixel_map.h
@@ -1,71 +1,108 @@
 #pragma once
 
-#include <vector>
+#if !__CUDACC__
 #include <ostream>
-
 #include "util/png_writer.h"
+#include "util/bstream.h"
+#endif
 
 namespace PET2D {
 
 /// Rectangular map of pixels aka 2D image
 ////
 /// Can be used to write pixels to PNG or generic stream
-template <typename PixelType, typename ValueType>
-class PixelMap : std::vector<ValueType> {
+template <typename PixelType, typename ValueType> class PixelMap {
  public:
   using Pixel = PixelType;
   using S = typename Pixel::S;
   using Size = typename Pixel::Size;
   using Value = ValueType;
-  using Base = std::vector<Value>;
-  using iterator = typename Base::iterator;
-  using const_iterator = typename Base::const_iterator;
+  using iterator = Value*;
+  using const_iterator = const Value*;
+
+  /// Creates new pixel map of given dimensions initialized to zero
+  PixelMap(S width, S height)
+      : size(static_cast<Size>(width) * height),
+        width(width),
+        height(height),
+        data(new Value[size]()),
+        owning(true) {}
 
   /// Creates new pixel map of given dimensions and default value
-  PixelMap(S width, S height, Value value = 0)
-      : Base(static_cast<Size>(width) * height, value),
+  PixelMap(S width, S height, const Value& value)
+      : size(static_cast<Size>(width) * height),
         width(width),
-        height(height) {}
+        height(height),
+        data(new Value[size]),
+        owning(true) {
+    assign(value);
+  }
+
+  /// Creates new pixel map of given dimensions and existing data
+  PixelMap(S width, S height, Value* data)
+      : size(static_cast<Size>(width) * height),
+        width(width),
+        height(height),
+        data(data),
+        owning(false) {}
+
   /// Copy constructor
   PixelMap(const PixelMap& other)
-      : Base(other), width(other.width), height(other.height) {}
+      : size(other.size),
+        width(other.width),
+        height(other.height),
+        data(other.owning ? new Value[size] : other.data),
+        owning(other.owning) {
+    if (owning) {
+      memcpy(data, other.data, size * sizeof(Value));
+    }
+  }
+
   /// Move constructor
   PixelMap(PixelMap&& other)
-      : Base(other), width(other.width), height(other.height) {}
-
-  PixelMap& operator=(const PixelMap& other) {
-    if (other.width != width || other.height != height)
-      throw("cannot assign pixel map of diffent size");
-    Base::operator=(other);
-    return *this;
-  }
-  PixelMap& operator=(PixelMap&& other) {
-    if (other.width != width || other.height != height)
-      throw("cannot assign pixel map of diffent size");
-    Base::operator=(other);
-    return *this;
+      : size(other.size),
+        width(other.width),
+        height(other.height),
+        data(other.data),
+        owning(other.owning) {
+    // make other object not to release its memory
+    other.owning = false;
   }
 
-  Value& operator[](const Pixel& pixel) { return this->at(pixel.index(width)); }
+  /// Destroys underlying data (only if it is managed)
+  ~PixelMap() {
+    if (owning && data) {
+      delete[] data;
+    }
+  }
+
+  Value& operator[](const Pixel& pixel) { return data[pixel.index(width)]; }
   const Value& operator[](const Pixel& pixel) const {
-    return this->at(pixel.index(width));
+    return data[pixel.index(width)];
   }
-  Value& operator[](Size index) { return this->at(index); }
-  const Value& operator[](Size index) const { return this->at(index); }
+  Value& operator[](Size index) { return data[index]; }
+  const Value& operator[](Size index) const { return data[index]; }
 
-  void assign(const Value& v) { Base::assign(this->size(), v); }
+  void assign(const Value& value) {
+    for (auto& element : *this) {
+      element = value;
+    }
+  }
 
-  iterator begin() { return Base::begin(); }
-  const_iterator begin() const { return Base::begin(); }
-  iterator end() { return Base::end(); }
-  const_iterator end() const { return Base::end(); }
+  iterator begin() { return data; }
+  const_iterator begin() const { return data; }
+  iterator end() { return &data[size]; }
+  const_iterator end() const { return &data[size]; }
 
+  const Size size;
   const S width;
   const S height;
+  Value* const data;
 
+#if !__CUDACC__
   friend util::png_writer& operator<<(util::png_writer& png,
                                       const PixelMap& map) {
-    png.write(map.width, map.height, map.data());
+    png.write(map.width, map.height, map.data);
     return png;
   }
 
@@ -83,6 +120,15 @@ class PixelMap : std::vector<ValueType> {
     }
     return out;
   }
+
+  friend util::obstream& operator<<(util::obstream& out, const PixelMap& map) {
+    out.write(map.data, map.size);
+    return out;
+  }
+#endif
+
+ private:
+  bool owning;  ///< are we owning the data pointer?
 };
 
 }  // PET2D
