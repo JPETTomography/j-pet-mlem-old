28a64b788dfce429851957f1fb16cbf047bbb80b
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-10-26 21:46:55 +0100

    cmake_clang-3.7.sh: Allow passing extra params

--- scripts/cmake_clang-3.7.sh
+++ scripts/cmake_clang-3.7.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 path=$(cd >/dev/null 2>&1 $(dirname $0); pwd)
-cmake \
+exec cmake \
 	-GNinja \
 	-DCUDA_HOST_COMPILER=$path/clang-legacy \
 	-DCMAKE_CXX_COMPILER=`which clang++-3.7` \
 	-DOpenMP_CXX_FLAGS='-fopenmp=libomp -I/usr/local/OpenMP-3.7.0-x86_64-apple-darwin14.4.0/include' \
-	-DCMAKE_EXE_LINKER_FLAGS=-L/usr/local/OpenMP-3.7.0-x86_64-apple-darwin14.4.0/lib
+	-DCMAKE_EXE_LINKER_FLAGS=-L/usr/local/OpenMP-3.7.0-x86_64-apple-darwin14.4.0/lib \
+	"$@"
