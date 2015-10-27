733dc0c1c5026f6ed935e8522b88bc85f5d932b1
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-10-27 14:29:21 +0100

    Doxygen: Generate test case list via doc_filter.pl

--- scripts/doc_filter.pl
+++ scripts/doc_filter.pl
@@ -6,14 +6,25 @@ my $fn = @ARGV == 1 ? $ARGV[0] : '<in>';
 my $ln = 1;
 
 while (<>) {
-  if (/^[ ]*\/\/\/ \\verboutput ([a-z0-9_]+)$/) {
+  if (/^[ ]*\/\/\/ \\verboutput ([a-z0-9_]+)((?:[ ]+[a-z0-9_-]+)*)$/) {
     # \verboutput some_cmd -> output of some_cmd wrapped in verbatim
     if (-x $1) {
-      my $out = `2>&1 ./$1`;
+      my $out = `2>&1 ./$1$2`;
       $out =~ s/^/\/\/\/     /gm;
       print "\/\/\/\n", $out;
     } else {
-      print STDERR "$fn:$ln: error: cannot execute `$1'\n";
+      print STDERR "$fn:$ln: error: cannot execute `$1$2'\n";
+    }
+  } elsif (/^[ ]*\/\/\/ \\listoutput ([a-z0-9_]+)((?:[ ]+[a-z0-9_-]+)*)$/) {
+    # \listoutput some_cmd -> output of some_cmd wrapped into list
+    if (-x $1) {
+      my $out = `2>&1 ./$1$2`;
+      $out =~ s/^/\/\/\/ /gm;
+      $out =~ s/^\/\/\/   (.*?)$/\/\/\/ * $1\n\/\/\//gm;
+      $out =~ s/^\/\/\/ (.*?):$/\/\/\/ ## $1/gm;
+      print "\/\/\/\n", $out;
+    } else {
+      print STDERR "$fn:$ln: error: cannot execute `$1$2'\n";
     }
   } elsif(/^[ ]*\/\//) {
     # //// -> empty line
