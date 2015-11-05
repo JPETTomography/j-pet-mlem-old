1fdf98d695a82b159b9637f5a577a942359296d2
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-11-05 23:42:44 +0100

    CUDA: Debug couple of other functions (incl. 3D)

--- src/util/cuda/debug.h
+++ src/util/cuda/debug.h
@@ -32,18 +32,24 @@ static cudaError cudbgLastError;
 
 // clang-format off
 #define cudaBindSurfaceToArray(...)   CUDBG(BindSurfaceToArray, __VA_ARGS__)
+#define cudaBindTexture2D(...)        CUDBG(BindTexture2D, __VA_ARGS__)
+#define cudaBindTextureToArray(...)   CUDBG(BindTextureToArray, __VA_ARGS__)
+#define cudaCreateChannelDesc(...)    CUDBG(CreateChannelDesc, __VA_ARGS__)
 #define cudaCreateTextureObject(...)  CUDBG(CreateTextureObject, __VA_ARGS__)
 #define cudaDestroyTextureObject(...) CUDBG(DestroyTextureObject, __VA_ARGS__)
 #define cudaFree(...)                 CUDBG(Free, __VA_ARGS__)
 #define cudaMalloc(...)               CUDBG(Malloc, __VA_ARGS__)
+#define cudaMalloc3DArray(...)        CUDBG(Malloc3DArray, __VA_ARGS__)
 #define cudaMallocArray(...)          CUDBG(MallocArray, __VA_ARGS__)
 #define cudaMallocPitch(...)          CUDBG(MallocPitch, __VA_ARGS__)
 #define cudaMemcpy(...)               CUDBG(Memcpy, __VA_ARGS__)
-#define cudaMemcpyToArray(...)        CUDBG(MemcpyToArray, __VA_ARGS__)
 #define cudaMemcpy2D(...)             CUDBG(Memcpy2D, __VA_ARGS__)
+#define cudaMemcpy3D(...)             CUDBG(Memcpy3D, __VA_ARGS__)
+#define cudaMemcpyToArray(...)        CUDBG(MemcpyToArray, __VA_ARGS__)
 #define cudaMemset(...)               CUDBG(Memset, __VA_ARGS__)
 #define cudaSetDevice(...)            CUDBG(SetDevice, __VA_ARGS__)
 #define cudaThreadSynchronize(...)    CUDBG(ThreadSynchronize, __VA_ARGS__)
+#define cudaUnbindTexture(...)        CUDBG(UnbindTexture, __VA_ARGS__)
 // clang-format on
 
 #endif
