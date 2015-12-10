525f51b046c81802945d0db75e4a6d4a4a375bc7
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2015-12-10 14:36:58 +0100

    Move PSF stuff into PSF class

--- src/3d/hybrid/psf_cmd.cpp
+++ src/3d/hybrid/psf_cmd.cpp
@@ -28,6 +28,7 @@
 #include "../geometry/vector.h"
 
 #include "options.h"
+#include "psf.h"
 
 using PixelGrid = PET2D::PixelGrid<F, S>;
 using VoxelGrid = PET3D::VoxelGrid<F, S>;
@@ -42,13 +43,6 @@ using Vector = PET3D::Vector<F>;
 #include <sys/mman.h>
 #else
 #include "util/bstream.h"
-#endif
-
-#if _OPENMP
-#include <omp.h>
-#else
-#define omp_get_max_threads() 1
-#define omp_get_thread_num() 0
 #endif
 
 void print_psf(const std::string& fn,
@@ -117,162 +111,6 @@ int main(int argc, char* argv[]) {
   CMDLINE_CATCH
 }
 
-inline void find_max(const VoxelMap& img, Voxel& max_voxel, F& max) {
-  Voxel thread_max_voxels[omp_get_max_threads()];
-  F thread_maxes[omp_get_max_threads()];
-  std::memset(thread_maxes, 0, sizeof(F) * omp_get_max_threads());
-#if _OPENMP
-#pragma omp parallel for schedule(dynamic)
-#endif
-  for (S z = 0; z < img.depth; ++z) {
-    Voxel l_max_voxel = thread_max_voxels[omp_get_thread_num()];
-    auto l_max = thread_maxes[omp_get_thread_num()];
-    for (S y = 0; y < img.height; ++y) {
-      for (S x = 0; x < img.width; ++x) {
-        const Voxel voxel(x, y, z);
-        const auto value = img[voxel];
-        if (value > l_max) {
-          l_max_voxel = voxel;
-          l_max = value;
-        }
-      }
-    }
-    thread_max_voxels[omp_get_thread_num()] = l_max_voxel;
-    thread_maxes[omp_get_thread_num()] = l_max;
-  }
-  max = 0;
-  for (int t = 0; t < omp_get_max_threads(); ++t) {
-    if (thread_maxes[t] > max) {
-      max = thread_maxes[t];
-      max_voxel = thread_max_voxels[t];
-    }
-  }
-}
-
-inline void find_left_right_above_half(const VoxelMap& img,
-                                       const Voxel max_voxel,
-                                       const F max,
-                                       Voxel& left_above_half,
-                                       Voxel& right_above_half) {
-  const auto half_max = max / 2;
-#if _OPENMP
-#pragma omp task shared(img, left_above_half, right_above_half)
-#endif
-  {
-    for (int x = 0; x <= max_voxel.x; ++x) {
-      Voxel voxel(x, max_voxel.y, max_voxel.z);
-      if (img[voxel] >= half_max) {
-        left_above_half.x = x;
-        break;
-      }
-    }
-    for (int x = img.width - 1; x >= max_voxel.x; --x) {
-      Voxel voxel(x, max_voxel.y, max_voxel.z);
-      if (img[voxel] >= half_max) {
-        right_above_half.x = x;
-        break;
-      }
-    }
-  }
-
-#if _OPENMP
-#pragma omp task shared(img, left_above_half, right_above_half)
-#endif
-  {
-    for (int y = 0; y <= max_voxel.y; ++y) {
-      Voxel voxel(max_voxel.x, y, max_voxel.z);
-      if (img[voxel] >= half_max) {
-        left_above_half.y = y;
-        break;
-      }
-    }
-    for (int y = img.height - 1; y >= max_voxel.y; --y) {
-      Voxel voxel(max_voxel.x, y, max_voxel.z);
-      if (img[voxel] >= half_max) {
-        right_above_half.y = y;
-        break;
-      }
-    }
-  }
-
-#if _OPENMP
-#pragma omp task shared(img, left_above_half, right_above_half)
-#endif
-  {
-    for (int z = 0; z <= max_voxel.z; ++z) {
-      Voxel voxel(max_voxel.x, max_voxel.y, z);
-      if (img[voxel] >= half_max) {
-        left_above_half.z = z;
-        break;
-      }
-    }
-    for (int z = img.depth - 1; z >= max_voxel.z; --z) {
-      Voxel voxel(max_voxel.x, max_voxel.y, z);
-      if (img[voxel] >= half_max) {
-        right_above_half.z = z;
-        break;
-      }
-    }
-  }
-
-#if _OPENMP
-#pragma omp taskwait
-#endif
-}
-
-inline void calculate_psf(const VoxelMap& img,
-                          const Voxel max_voxel,
-                          const F max,
-                          const Voxel left_above_half,
-                          const Voxel right_above_half,
-                          Vector& left,
-                          Vector& right,
-                          Vector& psf) {
-  const auto half_max = max / 2;
-
-  if (left_above_half.x == 0 || right_above_half.x == img.width - 1) {
-    psf.x = 0;
-    left.x = left_above_half.x;
-    right.x = right_above_half.x;
-  } else {
-    Voxel la(left_above_half.x - 0, max_voxel.y, max_voxel.z);
-    Voxel lb(left_above_half.x - 1, max_voxel.y, max_voxel.z);
-    Voxel ra(right_above_half.x + 0, max_voxel.y, max_voxel.z);
-    Voxel rb(right_above_half.x + 1, max_voxel.y, max_voxel.z);
-    left.x = left_above_half.x - (img[la] - half_max) / (img[la] - img[lb]);
-    right.x = right_above_half.x + (img[ra] - half_max) / (img[ra] - img[rb]);
-    psf.x = right.x - left.x;
-  }
-
-  if (left_above_half.y == 0 || right_above_half.y == img.height - 1) {
-    psf.y = 0;
-    left.y = left_above_half.y;
-    right.y = right_above_half.y;
-  } else {
-    Voxel la(max_voxel.x, left_above_half.y - 0, max_voxel.z);
-    Voxel lb(max_voxel.x, left_above_half.y - 1, max_voxel.z);
-    Voxel ra(max_voxel.x, right_above_half.y + 0, max_voxel.z);
-    Voxel rb(max_voxel.x, right_above_half.y + 1, max_voxel.z);
-    left.y = left_above_half.y - (img[la] - half_max) / (img[la] - img[lb]);
-    right.y = right_above_half.y + (img[ra] - half_max) / (img[ra] - img[rb]);
-    psf.y = right.y - left.y;
-  }
-
-  if (left_above_half.z == 0 || right_above_half.z == img.depth - 1) {
-    psf.z = 0;
-    left.z = left_above_half.z;
-    right.z = right_above_half.z;
-  } else {
-    Voxel la(max_voxel.x, max_voxel.y, left_above_half.z - 0);
-    Voxel lb(max_voxel.x, max_voxel.y, left_above_half.z - 1);
-    Voxel ra(max_voxel.x, max_voxel.y, right_above_half.z + 0);
-    Voxel rb(max_voxel.x, max_voxel.y, right_above_half.z + 1);
-    left.z = left_above_half.z - (img[lb] - half_max) / (img[la] - img[lb]);
-    right.z = right_above_half.z + (img[rb] - half_max) / (img[ra] - img[rb]);
-    psf.z = right.z - left.z;
-  }
-}
-
 void print_psf(const std::string& fn,
                const VoxelMap& img,
                const F s_pixel,
@@ -280,12 +118,12 @@ void print_psf(const std::string& fn,
   (void)img;
   Voxel max_voxel;
   F max;
-  find_max(img, max_voxel, max);
+  PET3D::Hybrid::PSF::find_max(img, max_voxel, max);
   Voxel left_above_half, right_above_half;
-  find_left_right_above_half(
+  PET3D::Hybrid::PSF::find_left_right_above_half(
       img, max_voxel, max, left_above_half, right_above_half);
   Vector left, right, psf;
-  calculate_psf(
+  PET3D::Hybrid::PSF::calculate(
       img, max_voxel, max, left_above_half, right_above_half, left, right, psf);
   out << std::setw(35) << fn << '\t'         //
       << std::setw(3) << std::fixed          //
