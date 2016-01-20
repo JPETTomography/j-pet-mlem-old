e49aca458764690f716cd335ef0ec4134017f085
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2016-01-20 17:46:34 +0100

    Use absolute path to Clang 3.7 on OS X

--- scripts/cmake_clang-3.7.sh
+++ scripts/cmake_clang-3.7.sh
@@ -4,7 +4,7 @@ path=$(cd >/dev/null 2>&1 $(dirname $0); pwd)
 exec cmake \
 	-GNinja \
 	-DCUDA_HOST_COMPILER=$path/clang-legacy \
-	-DCMAKE_CXX_COMPILER=`which clang++-3.7` \
+	-DCMAKE_CXX_COMPILER=/usr/local/clang+llvm-3.7.0-x86_64-apple-darwin/bin/clang++ \
 	-DOpenMP_CXX_FLAGS='-fopenmp=libomp -I/usr/local/OpenMP-3.7.0-x86_64-apple-darwin14.4.0/include' \
 	-DCMAKE_EXE_LINKER_FLAGS=-L/usr/local/OpenMP-3.7.0-x86_64-apple-darwin14.4.0/lib \
 	"$@"
