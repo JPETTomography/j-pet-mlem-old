c73e419e125c10907e0b7630246b9c5c5d066304
Piotr Bialas <pbialas7@gmail.com>
2016-02-15 14:19:17 +0100

    Refactoring kernel_monte_carlo_cmd.cpp.
    Moved soem of the functionality to separate kernel_monte_carlo.* files.

---
+++ src/2d/strip/kernel_monte_carlo.h
@@ -0,0 +1,11 @@
+#ifndef KERNEL_MONTE_CARLO
+#define KERNEL_MONTE_CARLO
+
+void strip_integral();
+void strip_integral_theta();
+void strip_integral_event();
+void strip_integral_theta_event();
+void strip_gauss_kernel();
+void strip_gauss_kernel_integral();
+
+#endif  // KERNEL_MONTE_CARLO
