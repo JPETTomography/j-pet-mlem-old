a724d9b2336249503e3f48f8a622e43b0e04114e
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2016-06-23 20:53:39 +0200

    Fix-up online docs cross-protocol script usage

--- scripts/sync_doc.sh
+++ scripts/sync_doc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec rsync -avu --delete doc/html/ sorbus.if.uj.edu.pl:Sites/
+rsync -avu --delete doc/html/ sorbus.if.uj.edu.pl:Sites/ && \
+ssh sorbus.if.uj.edu.pl 'sed -i \"\" -e s,src=\"http://,src=\"//,g Sites/*.html'
