4cfaa25e03c41aa8927817882c92b2b968ffcc70
Adam Strzelecki <adam.strzelecki@uj.edu.pl>
2016-08-12 16:14:49 +0200

    CUDA: Ensure kernels are run without errors #2

--- src/2d/barrel/cuda/matrix.cu
+++ src/2d/barrel/cuda/matrix.cu
@@ -137,6 +137,7 @@ void run<Scanner>(
            length_scale,
            rng_state,
            pixel_hits);
+    cudaPeekAtLastError();  // ensure kernel was run successfully
     cudaThreadSynchronize();
 
     pixel_hits.copy_from_device();
