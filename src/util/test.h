7d740a9d76815c7901fea3575705552031e0c113
Piotr Bialas <pbialas7@gmail.com>
2017-02-08 21:40:58 +0100

    Added VecApprox class to enable approximate equality check for Vector and Point in Catch.

--- src/util/test.h
+++ src/util/test.h
@@ -56,6 +56,83 @@ inline InverseApprox&& operator-(Approx&& other) {
   return static_cast<InverseApprox&&>(other);
 }
 
+template <typename Vec> class VecApprox {
+ public:
+  explicit VecApprox(const Vec& value)
+      : m_epsilon(std::numeric_limits<float>::epsilon() * 100),
+        m_scale(1.0),
+        m_value(value) {}
+
+  VecApprox(VecApprox const& other)
+      : m_epsilon(other.m_epsilon),
+        m_scale(other.m_scale),
+        m_value(other.m_value) {}
+
+  static VecApprox custom() { return VecApprox(Vec(0, 0, 0)); }
+
+  VecApprox operator()(double value) {
+    VecApprox approx(value);
+    approx.epsilon(m_epsilon);
+    approx.scale(m_scale);
+    return approx;
+  }
+
+  friend bool operator==(Vec lhs, VecApprox const& rhs) {
+    // Thanks to Richard Harris for his help refining this formula
+    bool x_check =
+        fabs(lhs.x - rhs.m_value.x) <
+        rhs.m_epsilon *
+            (rhs.m_scale + (std::max)(fabs(lhs.x), fabs(rhs.m_value.x)));
+    bool y_check =
+        fabs(lhs.y - rhs.m_value.y) <
+        rhs.m_epsilon *
+            (rhs.m_scale + (std::max)(fabs(lhs.y), fabs(rhs.m_value.y)));
+    bool z_check =
+        fabs(lhs.z - rhs.m_value.z) <
+        rhs.m_epsilon *
+            (rhs.m_scale + (std::max)(fabs(lhs.z), fabs(rhs.m_value.z)));
+
+    return x_check && y_check && z_check;
+  }
+
+  friend bool operator==(VecApprox const& lhs, Vec rhs) {
+    return operator==(rhs, lhs);
+  }
+
+  Approx& epsilon(double newEpsilon) {
+    m_epsilon = newEpsilon;
+    return *this;
+  }
+
+  Approx& scale(double newScale) {
+    m_scale = newScale;
+    return *this;
+  }
+
+  std::string toString() const {
+    std::ostringstream oss;
+    oss << "Approx( " << m_value << " )";
+    return oss.str();
+  }
+
+  template <typename V> friend VecApprox<V> VApprox(V vec);
+
+ private:
+  double m_epsilon;
+  double m_scale;
+  Vec m_value;
+};
+
+template <typename V> VecApprox<V> VApprox(V vec) { return VecApprox<V>(vec); }
+
+namespace Catch {
+template <typename Vec> struct StringMaker<VecApprox<Vec>> {
+  static std::string convert(const VecApprox<Vec>& vec) {
+    return vec.toString();
+  }
+};
+}
+
 // Stupid but working workaround for making syntax highlight working for tests
 // in QtCreator. Use TEST("...") instead TEST_CASE("...").
 #define TEST TEST_CASE
