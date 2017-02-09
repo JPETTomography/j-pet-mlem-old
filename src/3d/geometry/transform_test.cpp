5136137e696b313a57799efe033b96c184e97369
Piotr Bialas <pbialas7@gmail.com>
2017-02-09 14:14:41 +0100

    Second vector rotation test passing.

--- src/3d/geometry/transform_test.cpp
+++ src/3d/geometry/transform_test.cpp
@@ -11,8 +11,19 @@ using Vector = PET3D::Vector<F>;
 using Point = PET3D::Point<F>;
 
 TEST_CASE("3d/transform") {
-  Vector src(0.1f, 0.2f, 0.3f);
-  auto dest = rotate(src, F(M_PI / 3.0), Vector(0, 0, 1.0));
 
-  CHECK(dest == VApprox(Vector(-0.123205080757, 0.186602540378, 0.3)));
+  SECTION("vector/rotation/1") {
+    Vector src(0.1f, 0.2f, 0.3f);
+    auto dest = rotate(src, F(M_PI / 3.0), Vector(0, 0, 1.0));
+
+    CHECK(dest == VApprox(Vector(-0.123205080757, 0.186602540378, 0.3)));
+  }
+
+  SECTION("Vector/rotation/2") {
+    Vector src(0.1f, 0.2f, 0.3f);
+    auto dest = rotate(src, F(M_PI / 3.0), Vector(.2, .4, 1).normalized());
+
+    CHECK(dest ==
+          VApprox(Vector(0.02008778013, 0.198289443268, 0.316666666667)));
+  }
 }
