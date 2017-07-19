d9d1a866336f5258ec498a9120357db066342b57
Piotr Bialas <pbialas7@gmail.com>
2017-07-19 11:03:25 +0200

    Added tests for find_symmetric function.
    This function finds symmetric detector to a given one if exists.

--- src/2d/barrel/symmetry_descriptor_test.cpp
+++ src/2d/barrel/symmetry_descriptor_test.cpp
@@ -5,12 +5,19 @@
 
 #include "symmetry_descriptor.h"
 #include "2d/geometry/point.h"
+
+#include "scanner_builder.h"
+#include "generic_scanner.h"
+#include "square_detector.h"
+#include "2d/geometry/find_symmetry.h"
+
 #include "common/types.h"
 
+using Point = PET2D::Point<F>;
+using Transformation = PET2D::Transformation<F>;
+using SymmetryDescriptor = PET2D::Barrel::SymmetryDescriptor<S>;
+
 TEST("Symmetry transformations") {
-  using Point = PET2D::Point<F>;
-  using Transformation = PET2D::Transformation<F>;
-  using SymmetryDescriptor = PET2D::Barrel::SymmetryDescriptor<S>;
 
   std::vector<Transformation> tr;
   tr.reserve(SymmetryDescriptor::EIGHT);
@@ -30,4 +37,40 @@ TEST("Symmetry transformations") {
   REQUIRE(tr[5](p).approx_equal(Point(p.y, -p.x)));
   REQUIRE(tr[6](p).approx_equal(Point(-p.y, p.x)));
   REQUIRE(tr[7](p).approx_equal(Point(-p.y, -p.x)));
+}
+
+TEST("Find symmetry") {
+  using Builder = PET2D::Barrel::ScannerBuilder<
+      PET2D::Barrel::GenericScanner<PET2D::Barrel::SquareDetector<F>, S, 128>>;
+
+  auto detector = Builder::build_single_ring(200.0, 8, 0.007, 0.019);
+
+  auto symmetry_descriptor = detector.symmetry_descriptor();
+
+  // first ring
+  for (short d = 0; d < 8; d++)
+    REQUIRE(symmetry_descriptor.symmetric_detector(d, 0) == d);
+
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 1) == 7);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 2) == 3);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 3) == 5);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 4) == 1);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 5) == 3);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 6) == 7);
+  REQUIRE(symmetry_descriptor.symmetric_detector(1, 7) == 5);
+
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 1) == 2);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 2) == 6);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 3) == 2);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 4) == 4);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 5) == 0);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 6) == 4);
+  REQUIRE(symmetry_descriptor.symmetric_detector(6, 7) == 0);
+
+  for (S s = 0; s < SymmetryDescriptor::EIGHT; s++) {
+    for (S d = 0; d < detector.size(); d++) {
+      REQUIRE(find_symmetric(detector, s, d) ==
+              symmetry_descriptor.symmetric_detector(s, d));
+    }
+  }
 }
---
+++ src/2d/geometry/find_symmetry.h
@@ -0,0 +1,11 @@
+#ifndef FIND_SYMMETRY_H
+#define FIND_SYMMETRY_H
+
+template <typename Scanner>
+static typename Scanner::S find_symmetric(Scanner scanner,
+                                          typename Scanner::S symmetry,
+                                          typename Scanner::S detector) {
+  return 0;
+}
+
+#endif  // FIND_SYMMETRY_H
