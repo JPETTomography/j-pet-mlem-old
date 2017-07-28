6fb22465c692f9f0646caad0d3cd415d1ab4a7c5
Piotr Bialas <pbialas7@gmail.com>
2017-07-28 11:41:00 +0200

    Adding skeleton of the describe_scanner_cmd.cpp.

--- src/2d/barrel/describe_scanner_cmd.cpp
+++ src/2d/barrel/describe_scanner_cmd.cpp
@@ -1,4 +1,20 @@
 #include "cmdline.h"
 #include "util/cmdline_types.h"
+#include "util/cmdline_hooks.h"
+#include "util/backtrace.h"
 
-int main() { return 0; }
+int main(int argc, char* argv[]) {
+
+  CMDLINE_TRY
+
+  cmdline::parser cl;
+
+  cl.add<std::string>("output", 'o', "ouput file", true);
+  cl.add("--big-barrel", 'B', "creates big barrel description");
+  cl.add("--small-barrel", 'b', "creates small barrel description");
+  cl.add("--new-module", 'n', "new modules description");
+  cl.add("--full", 'f', "full detector: new + big barrel");
+
+  CMDLINE_CATCH
+  return 0;
+}
