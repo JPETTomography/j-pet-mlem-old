90f8a050ad10f901dec8b229e5eed038b88018af
Piotr Bialas <pbialas7@gmail.com>
2017-07-31 12:50:49 +0200

    Removed one test from gate_volume_test.cpp.
    The one that did not use the builder.

--- src/2d/gate/gate_volume_test.cpp
+++ src/2d/gate/gate_volume_test.cpp
@@ -288,68 +288,6 @@ TEST("2d Gate volume") {
     mgraphics.add(scanner);
   }
 
-  SECTION("new full") {
-    auto world = new Box(2, 2);
-
-    auto layer_new = new Cylinder(0.35, 0.4);
-    world->attach_daughter(layer_new);
-
-    auto module = new Box(0.026, 0.0085);
-    module->set_translation(Vector(0.37236, 0));
-    module->attach_repeater(new Gate::D2::Ring<F>(24, Vector(0.0, 0.0)));
-
-    layer_new->attach_daughter(module);
-
-    auto scintillator = new Box(0.024, 0.006);
-    scintillator->attach_repeater(
-        new Gate::D2::Linear<F>(13, Vector(0, 0.007)));
-    scintillator->attach_crystal_sd();
-
-    module->attach_daughter(scintillator);
-
-    auto layer_1 = new Cylinder(0.425 - 0.005, 0.425 + 0.005);
-    world->attach_daughter(layer_1);
-    auto scintillator_1 = new Box(0.021, 0.009);
-    scintillator_1->set_translation(Vector(0.425, 0));
-    scintillator_1->attach_repeater(new Gate::D2::Ring<F>(48, Vector(0, 0)));
-    scintillator_1->attach_crystal_sd();
-    layer_1->attach_daughter(scintillator_1);
-
-    auto layer_2 = new Cylinder(0.4675 - 0.005, 0.4675 + 0.005);
-    world->attach_daughter(layer_2);
-    auto scintillator_2 = new Box(0.021, 0.009);
-    scintillator_2->set_translation(Vector(0.4675, 0));
-    scintillator_2->attach_repeater(
-        new Gate::D2::Ring<F>(48, Vector(0, 0), M_PI / 48));
-    scintillator_2->attach_crystal_sd();
-    layer_2->attach_daughter(scintillator_2);
-
-    auto layer_3 = new Cylinder(0.575 - 0.005, 0.575 + 0.005);
-    world->attach_daughter(layer_3);
-    auto scintillator_3 = new Box(0.021, 0.009);
-    scintillator_3->set_translation(Vector(0.575, 0));
-    scintillator_3->attach_repeater(
-        new Gate::D2::Ring<F>(96, Vector(0, 0), M_PI / 96));
-    scintillator_3->attach_crystal_sd();
-    layer_3->attach_daughter(scintillator_3);
-
-    Gate::D2::GenericScannerBuilder<F, S, 512> builder;
-    PET2D::Barrel::GenericScanner<PET2D::Barrel::SquareDetector<F>, S, 512>
-        scanner(0.4, 0.8);
-    builder.build(world, &scanner);
-    REQUIRE(13 * 24 + 48 + 48 + 96 == scanner.size());
-
-    util::svg_ostream<F> out("new_full.svg", .9, .9, 1024., 1024l);
-    out << scanner;
-
-    std::ofstream mout("new_full.m");
-    Common::MathematicaGraphics<F> mgraphics(mout);
-    mgraphics.add(scanner);
-
-    auto n_detectors = builder.count_cristals(world);
-    REQUIRE(n_detectors == scanner.size());
-  }
-
   SECTION("new full from builder") {
 
     auto world = Gate::D2::build_new_full_scanner_volume<F>();
